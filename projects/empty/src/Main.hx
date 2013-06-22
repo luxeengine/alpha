b87dbad487cc74508c0ed677f705fabf48038ad2
Sven Bergström <Sven@Svens-MacBook-Pro.local>
2013-06-22 17:05:16 -0230

    Adding projects folder

---
+++ projects/empty/src/Main.hx
@@ -0,0 +1,16 @@
+
+import haxelab.Game;
+
+class Main extends Game {
+
+    public override function ready() {
+        
+        trace('HEY LOOK, I am a game');
+
+        //make sprite
+        //do things
+    }
+
+}
+
+
