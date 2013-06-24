50065d44bd04b301908d4b8ac068e7c7dbba6c11
underscorediscovery <fuzzyspoon@gmail.com>
2013-06-23 21:39:50 -0230

    Adding camera code and fixing render order due to tri stips

--- projects/empty/src/Main.hx
+++ projects/empty/src/Main.hx
@@ -4,21 +4,21 @@ import phoenix.geometry.QuadGeometry;
 
 
 class Main extends lab.Game {
-	
-	var geom : QuadGeometry;
+    
+    var geom : QuadGeometry;
 
     public function ready() {
-	   trace("I AM A GAME");
+       trace("I AM A GAME");
 
-	   geom = new QuadGeometry( { x:200, y:200, width:200, height:200 } );
-	   
-	   core.renderer.default_batcher.add(geom);
+       geom = new QuadGeometry( { x:200, y:200, width:200, height:200 } );
+       
+       core.renderer.default_batcher.add(geom);
 
-	}
+    }
 
     public function update() {
         geom.verts['tl'].pos.y -= 0.2;
-		geom.verts['tr'].pos.y -= 0.1;
+        geom.verts['tr'].pos.y -= 0.1;
     }
 
     public function shutdown() {
