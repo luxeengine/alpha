f8e43839798544054c1dc1da8ff5ec2b69c817b5
underscorediscovery <sven@underscorediscovery.com>
2013-07-20 17:29:49 -0230

    Adding BST tests

--- tests/BST.hx
+++ tests/BST.hx
@@ -1,12 +1,33 @@
 package tests;
 
+import tests.H;
 import phoenix.utils.BinarySearchTree;
 
+class TestPriority {
+	public var priority = 0;
+	public function new(p:Int = 0){
+		priority = p;
+	}
+	public function compare(other:TestPriority) {
+			//if i am less, -1
+		if(other.priority > priority) return -1;
+			//if i am more, +1
+		if(other.priority < priority) return 1;		
+			//if im equal or values equal, 0
+		return 0;
+	}
+
+	public function toString() {
+		return 'TestPriority(' + priority + ')';
+	}
+}
 class BST {
 	
 	var tree1 : BinarySearchTree<Int>;
 	var tree2 : BinarySearchTree<Float>;
 	var tree3 : BinarySearchTree<String>;
+	var tree4 : BinarySearchTree<TestPriority>;
+	var tree5 : BinarySearchTree<TestPriority>;
 
 	public function new() {
 			//test 1, default compare function, int
@@ -18,6 +39,16 @@ class BST {
 			//test 3, default compare function, string
 		tree3 = new BinarySearchTree<String>();
 		test_string(tree3);
+			//test 4, default compare function, object
+		tree4 = new BinarySearchTree<TestPriority>();
+		test_object(tree4);
+			//test 5, custom compare function, object
+		tree5 = new BinarySearchTree<TestPriority>(compare_objects);
+		test_object_sortable(tree5);
+	}
+
+	public function compare_objects<T>(obj1:T,obj2:T) {
+		return Reflect.callMethod(obj1,'compare',[obj2]);
 	}
 
 	public function print_tree<T>(_tree:BinarySearchTree<T>) {
@@ -29,45 +60,108 @@ class BST {
 			++_count;
 		}
 		string += ' ];';
-		trace(string);
+		// trace(string);
+		return string;
 	}
 
 	public function test_integers( _tree:BinarySearchTree<Int> ) {
-		trace("Testing integer sorting");
-		var list = [7,2,5,6,1,0,7,36,3];
-		for(item in list) {
-			trace('tree before - ');
-			print_tree(_tree);
-			_tree.insert(item);
-			trace('tree after - ' );
-				trace('\t\tinserted ' + item);
-					print_tree(_tree);				
-		}		
+		
+		H.it( 'should sort the list of integers correctly' , function(){
+
+			var list = [7,2,5,6,1,0,7,36,3];
+			var result = '';
+			var expected = '\ttree: [BST, size=9] [ 0 , 1 , 2 , 3 , 5 , 6 , 7 , 7 , 36 ];';
+
+			for(item in list) {
+				_tree.insert(item);
+			}
+
+			result = print_tree(_tree);
+			H.equal(result,expected);
+
+		});	//it
+
 	} //test_integers
 
 	public function test_float( _tree:BinarySearchTree<Float> ) {
-		trace("Testing floating point sorting");
-		var list = [0.2,2.7,5.9,5.6,5.1,0,2.7,36.2,3.54];
-		for(item in list) {
-			trace('tree before - ');
-			print_tree(_tree);
-			_tree.insert(item);
-			trace('tree after - ' );
-				trace('\t\tinserted ' + item);
-					print_tree(_tree);				
-		}		
+
+		H.it( 'should sort the list of floats correctly' , function(){
+
+			var list = [0.2,2.7,5.9,5.6,5.1,0,2.7,36.2,3.54];
+			var result = '';
+			var expected = '\ttree: [BST, size=9] [ 0 , 0.2 , 2.7 , 2.7 , 3.54 , 5.1 , 5.6 , 5.9 , 36.2 ];';
+
+			for(item in list) {
+				_tree.insert(item);
+			}
+
+			result = print_tree(_tree);
+			H.equal(result,expected);
+
+		}); //it
+
+
+
 	} //test_float
 
 	public function test_string( _tree:BinarySearchTree<String> ) {
-		trace("Testing string sorting");
-		var list = ['derp','able','good','all','able','fire','zero','one','bent','count','enquire'];
-		for(item in list) {
-			trace('tree before - ');
-			print_tree(_tree);
-			_tree.insert(item);
-			trace('tree after - ' );
-				trace('\t\tinserted ' + item);
-					print_tree(_tree);				
-		}		
-	} //test_float
+		
+		H.it( 'should sort the list of strings correctly' , function(){
+
+			var list = ['derp','able','good','all','able','fire','zero','one','bent','count','enquire'];
+			var result = '';
+			var expected = '\ttree: [BST, size=11] [ able , able , all , bent , count , derp , enquire , fire , good , one , zero ];';
+			
+			for(item in list) {
+				_tree.insert(item);
+			}
+
+			result = print_tree(_tree);	
+			H.equal(result, expected);
+
+		});//it
+
+	} //test_string
+
+	public function test_object( _tree:BinarySearchTree<TestPriority> ) {
+
+		H.it( 'should NOT sort the list of objects correctly' , function(){
+		
+			var list = [new TestPriority(7),new TestPriority(3),
+						new TestPriority(12),new TestPriority(2),
+						new TestPriority(7),new TestPriority(1) ];
+			var result = '';
+			var expected = '\ttree: [BST, size=6] [ TestPriority(7) , TestPriority(3) , TestPriority(12) , TestPriority(2) , TestPriority(7) , TestPriority(1) ];';
+
+			for(item in list) {
+				_tree.insert(item);
+			}
+
+			result = print_tree(_tree);
+			H.equal(result, expected);
+
+		});//it
+
+	} //test_priority
+
+	public function test_object_sortable( _tree:BinarySearchTree<TestPriority> ) {
+
+		H.it( 'should sort the list of objects correctly' , function(){
+		
+			var list = [new TestPriority(7),new TestPriority(3),
+						new TestPriority(12),new TestPriority(2),
+						new TestPriority(7),new TestPriority(1) ];
+			var result = '';
+			var expected = '\ttree: [BST, size=6] [ TestPriority(1) , TestPriority(2) , TestPriority(3) , TestPriority(7) , TestPriority(7) , TestPriority(12) ];';
+
+			for(item in list) {
+				_tree.insert(item);
+			}
+
+			result = print_tree(_tree);
+			H.equal(result, expected);
+
+		});//it
+
+	} //test_priority
 }
