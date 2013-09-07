89c001d28a21da97369e2f255ef617d9e7f9a976
underscorediscovery <sven@underscorediscovery.com>
2013-09-07 16:32:23 -0230

    Adding linked list based stack structure

---
+++ luxe/structures/Stack.hx
@@ -0,0 +1,76 @@
+package luxe.structures;
+
+class Stack<T> {
+
+        //only editable by this class, gettable anywhere
+	@:isVar public var length (default, null) : Int = 0;
+
+	public var first : StackNode<T>;
+
+	public function new() {
+		first = null;
+		length = 0;
+	} //new
+
+	public function empty() {
+		return first == null;
+	} //empty
+
+	public function push( element:T ) {
+
+		if(element==null) throw "This Stack does not hold null elements.";
+
+		var oldfirst : StackNode<T> = first;
+
+        first = new StackNode<T>();
+
+        	first.item = element;
+        	first.next = oldfirst;
+
+        length++;
+
+	} //push
+
+	public function pop() : T {
+        if (empty()) throw "Stack underflow";
+        
+        var element : T = first.item;
+        	first = first.next;
+        
+        length--;
+
+        return element;
+    } //pop
+
+    public function top() : T {
+        if (empty()) return null;
+        return first.item;
+    } //top
+
+    public function iterator() : Iterator<T> {
+    	return toArray().iterator();
+    } //iterator
+
+    public function toArray() : Array<T> {
+    	
+    	if(empty()) return [];
+    		//start at the top of the stack
+    	var a : Array<T> = [];
+    	var current = first;
+    	while(current != null) {
+    		a.push( current.item );
+    		current = current.next;
+    	}
+
+    	return a;
+
+    } //toArray
+
+}
+
+
+class StackNode<T> {
+	public var item : T;
+	public var next : StackNode<T>;
+    public function new() {}
+}
---
+++ tests/test_stack/src/Main.hx
@@ -0,0 +1,48 @@
+
+import luxe.Vector;
+import luxe.Input;
+import luxe.structures.Stack;
+
+class Main extends luxe.Game {
+
+
+    public function ready() {
+
+        var s = new Stack<String>();
+
+        s.push('last');
+        s.push('secondlast');
+        s.push('fifth');
+        s.push('fourth');
+        s.push('third');
+        s.push('second');
+        s.push('first');
+
+        Luxe.core.show_console(true);
+
+        trace("stack should have 7 elements, it has " + s.length);
+        trace("they are : ");
+        
+        for(_s in s) {
+            trace(_s);
+        }
+
+
+    } //ready
+  
+    public function onkeyup(e) {
+        if(e.value == Input.Keys.escape) {
+            Luxe.shutdown();
+        }
+    } //onkeyup
+
+    public function update(dt:Float) {
+
+    } //update
+
+    public function shutdown() {
+
+    } //shutdown
+}
+
+
