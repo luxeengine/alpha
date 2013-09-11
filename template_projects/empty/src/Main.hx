cbee722d0c79f6e6916e0cf8def237175357eb5c
underscorediscovery <sven@underscorediscovery.com>
2013-09-11 18:13:40 -0230

    Switching to project.luxe.xml

---
+++ template_projects/empty/src/Main.hx
@@ -0,0 +1,26 @@
+
+import luxe.Vector;
+import luxe.Input;
+
+class Main extends luxe.Game {
+
+    public function ready() {
+
+    } //ready
+  
+    public function onkeyup(e) {
+        if(e.value == Input.Keys.escape) {
+            Luxe.shutdown();
+        }
+    } //onkeyup
+
+    public function update(dt:Float) {
+
+    } //update
+
+    public function shutdown() {
+
+    } //shutdown
+}
+
+
