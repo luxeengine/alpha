d95fe6317a6638fa65d2b26191271afe08d80e46
underscorediscovery <sven@underscorediscovery.com>
2013-09-22 14:51:46 -0230

    Updating project

--- projects/tdgame/src/Main.hx
+++ projects/tdgame/src/Main.hx
@@ -1,5 +1,7 @@
 
+import luxe.components.cameras.FlyCamera;
 import luxe.components.render.MeshComponent;
+
 import luxe.Entity;
 import luxe.Mesh;
 import luxe.Vector;
@@ -8,6 +10,8 @@ import phoenix.Camera;
 import phoenix.Matrix4;
 import phoenix.Texture;
 import phoenix.utils.Maths;
+
+//import luxe.components.physics.three.MeshCollider;
 
 class Main extends luxe.Game {
 
@@ -20,11 +24,13 @@ class Main extends luxe.Game {
     var tower_texture : Texture;
     var floor_texture : Texture;
     var camera : phoenix.Camera;
+    var flycam : FlyCamera;
 
     var left:Bool = false;
     var right:Bool = false;
     var forward:Bool = false;
     var back:Bool = false;
+    var fly:Bool = false;
 
     public function ready() {
 
@@ -39,9 +45,16 @@ class Main extends luxe.Game {
 
         floor = Luxe.scene.create(Entity, 'floor');
 
+        var floormesh = new Mesh({
+            file : 'assets/tower1.obj',
+            texture : floor_texture
+        });
+
         floor_mesh = floor.add(MeshComponent, 'mesh');
-            floor_mesh.file = 'assets/tower1.obj';
-            floor_mesh.texture = floor_texture;
+            floor_mesh.mesh =  floormesh;
+
+        // var mc = floor.add( MeshCollider, 'collider');
+        //     mc.mesh = floormesh;
 
         camera = new phoenix.Camera({
             projection: ProjectionType.perspective,
@@ -50,8 +63,18 @@ class Main extends luxe.Game {
             aspect:Luxe.screen.w/Luxe.screen.h
         });
 
+        flycam = new FlyCamera({
+            projection: ProjectionType.perspective,
+            fov:60, 
+            near:0.01,
+            aspect:Luxe.screen.w/Luxe.screen.h
+        });
+
         camera.pos = new Vector(0,4,3);
         camera.rotation.setFromEuler(new Vector(Maths.degToRad(-60),0,0));
+
+        flycam.view.pos = new Vector(0,4,3);
+        flycam.view.rotation.setFromEuler(new Vector(Maths.degToRad(-60),0,0));
 
         Luxe.renderer.default_batcher.view = camera;
 
@@ -63,6 +86,8 @@ class Main extends luxe.Game {
         Luxe.input.add('forward', KeyValue.key_W);
         Luxe.input.add('back', KeyValue.down);
         Luxe.input.add('back', KeyValue.key_S);
+        
+        Luxe.input.add('change_camera', KeyValue.key_C);
 
     } //ready
 
@@ -77,78 +102,96 @@ class Main extends luxe.Game {
         camera.pos.y += forward.y * dir;
     }
 
-    public function get_camera_point(?start:Vector=null) : Vector {
-        if(start == null) start = camera.pos;
-
-        var direction = new Vector(0,0,-1);
-        var rotmat = new Matrix4().makeRotationFromQuaternion(camera.rotation);
-            direction.applyMatrix4( rotmat );
-            direction.normalize();
-
-            //T = [planeNormal•(pointOnPlane - rayOrigin)]/planeNormal•rayDirection;
-            //pointInPlane = rayOrigin + (rayDirection * T);
-            var planeNormal = new Vector(0,-1,0);
-            var pointOnPlane = new Vector();
-            var rayOrigin = start;
-                //for [ ] 
-            var part1 = planeNormal.dot( Vector.Subtract(pointOnPlane, rayOrigin) );
-            var part2 = planeNormal.dot( direction );
-            var T = part1 / part2;
-
-            var scaled_direction = direction.multiplyScalar(T);
-            var pointInPlane = Vector.Add(start, scaled_direction);
-
-            return pointInPlane;
+    function mouse_in_world(p:Vector) {
+        var x = ( p.x / Luxe.screen.w ) * 2 - 1;
+        var y = -( p.y / Luxe.screen.h ) * 2 + 1;
+        return new Vector(x,y,1);
     }
 
-    // Convert mouse position to 3D
-    function screenToWorld(point:Vector) : Vector {
+    function unprojectVector( vector:Vector ) {
 
-        // Find near and far plane intersections
-        var point3f = new Vector(point.x, Luxe.screen.w * 0.5 - point.y, 0.0);
-        var nearPlane = unproject(point3f);
-        var farPlane = unproject(new Vector(point3f.x, point3f.y, 1.0));
+        var _viewProjectionMatrix = new Matrix4();
+        var _projectionMatrixInverse = new Matrix4();
 
-        // Calculate X, Y and return point
-        var theta = (0.0 - nearPlane.z) / (farPlane.z - nearPlane.z);
-        return new Vector(
-            nearPlane.x + theta * (farPlane.x - nearPlane.x), 
-            nearPlane.y + theta * (farPlane.y - nearPlane.y), 
-            0.0
-            );
+        _projectionMatrixInverse.getInverse( camera.projection_matrix );
+        _viewProjectionMatrix.multiplyMatrices( camera.modelview_matrix, _projectionMatrixInverse );
+
+        return vector.applyProjection( _viewProjectionMatrix );
 
     }
 
-    // Unproject a coordinate back to to camera
-    function unproject(point:Vector) {
+    // function rayIntersectTriangle( p1:Vector,p2:Vector,p3:Vector,backfaceCulling:Bool ) {
 
-        // Find the inverse Modelview-Projection-Matrix
-        var _mInvMVP : Matrix4 = camera.projection_matrix.clone().multiply(camera.modelview_matrix); //mProjection * mModelView;
-        var mInvMVP : Matrix4 = new Matrix4();
-        mInvMVP = mInvMVP.getInverse(_mInvMVP);
+    //         // from http://www.geometrictools.com/LibMathematics/Intersection/Wm5IntrRay3Triangle3.cpp
 
-            // Transform to normalized coordinates in the range [-1, 1]
+    //         edge1.subVectors( b, a );
+    //         edge2.subVectors( c, a );
+    //         normal.crossVectors( edge1, edge2 );
 
-        var pointNormal = new Vector();
-            pointNormal.x = (point.x - 0) / Luxe.screen.w * 2.0 - 1.0;
-            pointNormal.y = (point.y - 0) / Luxe.screen.h * 2.0;
-            pointNormal.z = 2.0 * point.z - 1.0;
-            pointNormal.w = 1.0;
+    //         // Solve Q + t*D = b1*E1 + b2*E2 (Q = kDiff, D = ray direction,
+    //         // E1 = kEdge1, E2 = kEdge2, N = Cross(E1,E2)) by
+    //         //   |Dot(D,N)|*b1 = sign(Dot(D,N))*Dot(D,Cross(Q,E2))
+    //         //   |Dot(D,N)|*b2 = sign(Dot(D,N))*Dot(D,Cross(E1,Q))
+    //         //   |Dot(D,N)|*t = -sign(Dot(D,N))*Dot(Q,N)
+    //         var DdN = this.direction.dot( normal );
+    //         var sign;
 
-        // Find the object's coordinates
-        var pointCoord : Vector = pointNormal.applyMatrix4(mInvMVP);
-        if (pointCoord.w != 0.0)
-            pointCoord.w = 1.0 / pointCoord.w;
+    //         if ( DdN > 0 ) {
 
-        // Return coordinate
-        return new Vector(
-                pointCoord.x * pointCoord.w, 
-                pointCoord.y * pointCoord.w, 
-                pointCoord.z * pointCoord.w
-            );
+    //             if ( backfaceCulling ) return null;
+    //             sign = 1;
 
-    }
+    //         } else if ( DdN < 0 ) {
 
+    //             sign = - 1;
+    //             DdN = - DdN;
+
+    //         } else {
+
+    //             return null;
+
+    //         }
+
+    //         diff.subVectors( this.origin, a );
+    //         var DdQxE2 = sign * this.direction.dot( edge2.crossVectors( diff, edge2 ) );
+
+    //         // b1 < 0, no intersection
+    //         if ( DdQxE2 < 0 ) {
+
+    //             return null;
+
+    //         }
+
+    //         var DdE1xQ = sign * this.direction.dot( edge1.cross( diff ) );
+
+    //         // b2 < 0, no intersection
+    //         if ( DdE1xQ < 0 ) {
+
+    //             return null;
+
+    //         }
+
+    //         // b1+b2 > 1, no intersection
+    //         if ( DdQxE2 + DdE1xQ > DdN ) {
+
+    //             return null;
+
+    //         }
+
+    //         // Line intersects triangle, check if ray does.
+    //         var QdN = - sign * diff.dot( normal );
+
+    //         // t < 0, no intersection
+    //         if ( QdN < 0 ) {
+
+    //             return null;
+
+    //         }
+
+    //         // Ray intersects triangle.
+    //         return this.at( QdN / DdN, optionalTarget );
+    
+    // }
 
     public function oninputdown( name:String, e:Dynamic ) {
         switch(name) {
@@ -156,6 +199,19 @@ class Main extends luxe.Game {
             case 'right': right = true;
             case 'forward': forward = true;
             case 'back': back = true;
+            case 'change_camera': 
+                fly = !fly;
+                if(fly) {
+                    trace('fly cam activated');
+                    Luxe.renderer.default_batcher.view = flycam.view;
+                    Luxe.scene.add(flycam);
+                } else {
+                    trace('game cam activated');
+                    Luxe.renderer.default_batcher.view = camera;
+                    Luxe.scene.remove(flycam);
+                    Luxe.showCursor(true);
+                    Luxe.lockCursor(false);
+                }
         }
     }
      public function oninputup( name:String, e:Dynamic ) {
@@ -168,31 +224,47 @@ class Main extends luxe.Game {
      }
 
     public function onmousedown( e:MouseEvent ) {
-        if(e.button == MouseButton.wheel_down || e.button == MouseButton.wheel_up) {
-            switch(e.button) {
-                case MouseButton.wheel_down:
-                    zoom(-1);
-                case MouseButton.wheel_up:
-                    zoom(1);
-                default:
+
+        if(!fly) {
+            if(e.button == MouseButton.wheel_down || e.button == MouseButton.wheel_up) {
+                switch(e.button) {
+                    case MouseButton.wheel_down:
+                        zoom(-1);
+                    case MouseButton.wheel_up:
+                        zoom(1);
+                    default:
+                }
             }
         }
+
     }
 
      public function onmouseup( e:MouseEvent ) {
 
-        // camera.onmouseup(e);
+        if(fly) {
+            flycam.onmouseup(e);
+        }
 
     } //onmouseup
   
     public function onmousemove( e:MouseEvent ) {
-        // camera.onmousemove(e);
-        var mp_world = screenToWorld(e.pos);
-        tower.pos = get_camera_point( camera.pos.clone().add(mp_world) );
+
+        var mdir = mouse_in_world(e.pos);
+            unprojectVector(mdir);
+            mdir.subtract(camera.pos);
+            mdir.normalize();
+
+        //now mdir is the starting point of the ray
+
+        if(fly) {
+            flycam.onmousemove(e);
+        }
     }
 
     public function onkeydown( e:KeyEvent ) {
-        // camera.onkeydown(e);
+        if(fly) {
+            flycam.onkeydown(e);
+        }
     }
 
     public function onkeyup( e:KeyEvent ) {
@@ -201,19 +273,21 @@ class Main extends luxe.Game {
             Luxe.shutdown();
         }
 
-        // camera.onkeyup(e);
+        if(fly) {
+            flycam.onkeyup(e);
+        }
 
     } //onkeyup
 
     var spd : Float = 2;
     public function update(dt:Float) {
 
-        if(forward)     { camera.pos.z -= spd * dt; }
-        if(back)        { camera.pos.z += spd * dt; }
-        if(left)        { camera.pos.x -= spd * dt; }
-        if(right)       { camera.pos.x += spd * dt; }
-
-        
+        if(!fly) {
+            if(forward)     { camera.pos.z -= spd * dt; }
+            if(back)        { camera.pos.z += spd * dt; }
+            if(left)        { camera.pos.x -= spd * dt; }
+            if(right)       { camera.pos.x += spd * dt; }
+        }
 
     } //update
 
