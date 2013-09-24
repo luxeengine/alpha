3a6227e5fac0f54cfa04894bd9a841bec7504f51
underscorediscovery <sven@underscorediscovery.com>
2013-09-24 14:47:23 -0230

    Cleanup loading race conditions on html5, and making sure things are scaled on create etc

--- tests/test_shaders/src/Main.hx
+++ tests/test_shaders/src/Main.hx
@@ -7,73 +7,88 @@ import phoenix.Shader;
 
 class Main extends luxe.Game {
 
-	var sprite1 : Sprite;
-	var sprite2 : Sprite;
-	var sprite3 : Sprite;
+	var distort_sprite : Sprite;
+	var hue_sprite : Sprite;
+	var level_sprite : Sprite;
 
-	var shader1 : Shader;
-	var shader2 : Shader;
-	var shader3 : Shader;
+	var level_tiltshift : Shader;
+	var hue_shader : Shader;
+	var distort_shader : Shader;
 
 	var loaded : Bool = false;
+	var loaded_logo : Bool = false;
 
     public function ready() {
 
-    	var tex = Luxe.loadTexture('assets/luxe.png');
-    	var tex2 = Luxe.loadTexture('assets/level.png');
-    	var tex3 = Luxe.loadTexture('assets/distort.png');
-    		tex2.filter = phoenix.Texture.FilterType.nearest;    	
+    	var luxe_tex = Luxe.loadTexture('assets/luxe.png');
+    	
+    	var level_texture = Luxe.loadTexture('assets/level.png');
+    		level_texture.filter = phoenix.Texture.FilterType.nearest;
 
-    	var sprite1 = new Sprite({
-    		texture : tex,
+    	var distort_map = Luxe.loadTexture('assets/distort.png');
+    		
+
+    	distort_sprite = new Sprite({
+    		texture : luxe_tex,
     		pos : new Vector(150,320)
     	});
 
-    	var sprite2 = new Sprite({
-    		texture : tex,
+    	hue_sprite = new Sprite({
+    		texture : luxe_tex,
     		pos : new Vector(430,320)
     	});
 
-    	var sprite3 = new Sprite({
-    		texture : tex2,
+    	level_sprite = new Sprite({
+    		texture : level_texture,
     		pos : new Vector(480,320)
     	});
 
-    	tex3.onload = function(tt) {
+    		//for the logo blocks image
+    	luxe_tex.onload = function(tt) {
 
-    		sprite1.scale = new Vector(0.5,0.5);
-	    	sprite2.scale = new Vector(0.5,0.5);
-	    	sprite3.scale = new Vector(4,4);
+    		distort_sprite.scale = new Vector(0.5,0.5);
+	    	hue_sprite.scale = new Vector(0.5,0.5);
 
-	    		//we do this separately so we can set the uniforms
-	    	shader1 = Luxe.loadShader('assets/gray_tilt_shift.glsl');
-	    	shader2 = Luxe.loadShader('assets/huechange.glsl');
-	    	shader3 = Luxe.loadShader('assets/distort.glsl');
+	    	hue_shader = Luxe.loadShader('assets/huechange.glsl');
+	    	hue_sprite.shader = hue_shader;
 
-	    	sprite3.shader = shader1;
-	    	sprite2.shader = shader2;
-	    	sprite1.shader = shader3;
+	    	loaded_logo = true;
+    	} //luxe_tex on load
+
+    	level_texture.onload = function(tt) {
+    		level_sprite.scale = new Vector(4,4);
+    		level_tiltshift = Luxe.loadShader('assets/gray_tilt_shift.glsl');
+	    	level_sprite.shader = level_tiltshift;
+    	} //level tex on load
+
+    	distort_map.onload = function(tt) {
+	    	
+	    	distort_shader = Luxe.loadShader('assets/distort.glsl');
+	    	distort_sprite.shader = distort_shader;
 
 	    		//move to second slot
-	    	tex3.slot = 1;
+	    	distort_map.slot = 1;
 	    		//set the uniform
-	    	shader3.set_uniform_texture('tex1', tex3);
+	    	distort_shader.set_uniform_texture('tex1', distort_map);
 
 	    	loaded = true;
-    	}	
 
+    	}	
 
 
     } //ready
 
     public function onmousemove( e:MouseEvent ) {
 
-    	if(loaded) {
+    	if(loaded && loaded_logo) {
+
 	    	var percent = e.pos.x / Luxe.screen.w;
     		var hue = (Math.PI*2) * percent;
 
-    		shader3.set_uniform_float('distortamount', percent);
-    		shader2.set_uniform_float('in_hue', hue);
+    			//distort based on mouse x
+    		distort_shader.set_uniform_float('distortamount', percent);
+    			//hue based on mouse x 
+    		hue_shader.set_uniform_float('in_hue', hue);
     	}
 
     }
