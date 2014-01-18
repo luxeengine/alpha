9b8a87e766d5d3277951fc661f56e4d3f6304b8c
underscorediscovery <sven@underscorediscovery.com>
2014-01-18 19:14:33 -0330

    Tests; base64 using the api instead

--- tests/test_base64/src/Main.hx
+++ tests/test_base64/src/Main.hx
@@ -1,13 +1,12 @@
 
 import luxe.Input;
-import luxe.utils.Base64;
 
 class Main extends luxe.Game {
 
     public function ready() {
 
-    	var encoded = Base64.encode('hello world');
-        var decoded = Base64.decode( encoded );
+    	var encoded = Luxe.utils.base64_encode('hello world');
+        var decoded = Luxe.utils.base64_decode( encoded );
 
         Luxe.draw.text({
         	text: "from hello world to `" + encoded + "` and back to `" + decoded +"`",
