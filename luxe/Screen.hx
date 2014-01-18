1c61956a6dcc1d68dc7fbbeada81128cd3a974ac
underscorediscovery <sven@underscorediscovery.com>
2014-01-18 17:41:44 -0330

    API; Screen; Adding Luxe.screen.mid for easy access to that

--- luxe/Screen.hx
+++ luxe/Screen.hx
@@ -11,17 +11,19 @@ class Screen {
 	@:isVar public var mid (get,null) : Vector;
 
 	public function new( _x:Int, _y:Int, _w:Int, _h:Int ) {
+		
 		x = _x;
 		y = _y;
 		w = _w;
 		h = _h;
 
 		mid = new Vector( Math.round(w/2), Math.round(h/2) );
-	}
+
+	} //new
 
 	function get_mid() : Vector {
 		return mid.clone();
-	} 
+	} //get_mid
 
     public function point_inside(_p:Vector) {
         if(_p.x < x) return false;
@@ -29,5 +31,6 @@ class Screen {
         if(_p.x > x+w) return false;
         if(_p.y > y+h) return false;
         return true;
-    }
-}
+    } //point_inside
+
+} //Screen
