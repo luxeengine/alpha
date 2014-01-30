c66de8dc94231b20e33fb184144565c382eed999
underscorediscovery <sven@underscorediscovery.com>
2014-01-29 23:38:58 -0330

    Audio; remap api to latest lime. REQUIRES latest lime repo code for now...

--- luxe/Audio.hx
+++ luxe/Audio.hx
@@ -5,49 +5,63 @@ import luxe.Core;
 class Audio {
     
     @:noCompletion public var core : Core;
-    @:noCompletion public function new( _core:Core ) { core = _core; }
+    
+    @:noCompletion public function new( _core:Core ) {
+
+        core = _core;
+
+    } //new
 
     @:noCompletion public function init() {
-
         core._debug(':: luxe :: \t Audio Initialized.');
-    }
+    } //init
 
     @:noCompletion public function destroy() {
         core._debug(':: luxe :: \t Audio shut down.');
-    }
+    } //destroy
 
     public function create(_name:String,_file:String, ?_forcemusic:Bool = false ) {
-        core.lime.audio.create(_name, _file, _forcemusic);
-    }
+        return core.lime.audio.create( _name, _file, _forcemusic );
+    } //create
 
-    public function play(_name:String, ?_loops:Int=0, ?_start:Float = 0) {
-        var s = core.lime.audio.get(_name);
-        if(s != null) {
-            s.play( _loops , _start );
-        } else {
-            trace('Audio :: Sound does not exist, use Luxe.audio.create() first : ' + _name);
-        }
-    }
+    public function sound( _name:String ) {
+        return core.lime.audio.sound(_name);
+    } //sound
 
-    public function volume(_name:String, _v:Float) {
-        var s = core.lime.audio.get(_name);
-        if(s != null) {
-            s.volume = _v;
-        } else {
-            trace('Audio :: Sound does not exist, use Luxe.audio.create() first : ' + _name);
-        }
-    }
+    public function exists( _name:String ) : Bool { 
+        return core.lime.audio.exists(_name);
+    } //exists
 
-    public function pan(_name:String, _p:Float) {
-        var s = core.lime.audio.get(_name);
-        if(s != null) {
-            s.pan = _p;
-        } else {
-            trace('Audio :: Sound does not exist, use Luxe.audio.create() first : ' + _name);
-        }
-    }
+    public function playing( _name:String ) {
+        return core.lime.audio.playing( _name );
+    } //playing
+
+        //will loop a sound indefinitely until stop is called
+    public function loop( _name:String ) {
+        return core.lime.audio.loop(_name);
+    } //loop
+
+    public function stop( _name:String ) {
+        return core.lime.audio.stop(_name);
+    } //stop
+
+    public function play( _name:String, ?_number_of_times:Int=1, ?_start_position_in_ms:Float = 0 ) {
+        return core.lime.audio.play(_name,_number_of_times, _start_position_in_ms);
+    } //play
+
+    public function volume( _name:String, ?_volume:Float = null ) : Float {
+        return core.lime.audio.volume(_name, _volume);
+    } //volume
+
+    public function pan( _name:String, ?_pan:Float = null ) : Float  {
+        return core.lime.audio.pan(_name,_pan);       
+    } //pan
+
+    public function position( _name:String, ?_pos:Float = null ) : Float {
+        return core.lime.audio.position( _name, _pos );       
+    } //position
 
     @:noCompletion public function process() {
         
-    }    
+    } //process
 }
--- tests/test_audio/src/Main.hx
+++ tests/test_audio/src/Main.hx
@@ -3,24 +3,48 @@ import luxe.Input;
 
 class Main extends luxe.Game {
 
+    var pan : Float = 1;
+
     public function ready() {
 
         Luxe.audio.create('sound', 'assets/sound.ogg');
         Luxe.audio.create('music', 'assets/ambience.ogg');
 
-        Luxe.audio.play('music');
+        Luxe.audio.loop('music');
 
+        // //pan from left to right and back
+        // luxe.tween.Actuate.tween(this, 20, { pan:-1 })
+        //     .repeat()
+        //     .reflect()
+        //     .onUpdate(function(){
+        //         Luxe.audio.pan('music', pan);
+        //     });
         
     } //ready
 
     public function onmouseup( e:MouseEvent ) {
-        Luxe.audio.play('sound');
+        if(e.button == MouseButton.left) {
+            Luxe.audio.play('sound', 3);
+        } else {
+            Luxe.audio.play('sound');
+        }        
     }
 
     public function onkeyup( e:KeyEvent ) {
 
         if(e.key == KeyValue.key_R) {
-            //
+            Luxe.audio.position('music', 0);
+        }
+
+        if(e.key == KeyValue.key_L) {
+            Luxe.audio.play('sound', 2);
+        }
+
+        if(e.key == KeyValue.key_S) {
+            Luxe.audio.stop('music');
+        }
+        if(e.key == KeyValue.key_P) {
+            Luxe.audio.play('music');
         }
 
         if(e.key == KeyValue.escape) {
@@ -29,7 +53,7 @@ class Main extends luxe.Game {
         
     } //onkeyup
 
-    public function update(dt:Float) {
+    public function update(dt:Float) {        
 
     } //update
 
