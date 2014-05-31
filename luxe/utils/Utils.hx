9085b90dc3af134d3370d6174daa0ff7cf0c206d
underscorediscovery <sven@underscorediscovery.com>
2014-05-31 14:15:12 -0230

    Utils; Making relative path a tad more useful on windows

--- luxe/utils/Utils.hx
+++ luxe/utils/Utils.hx
@@ -69,6 +69,7 @@ class Utils {
         
         return _path.charAt(0) != "#"
           && _path.charAt(0) != "/"
+          && _path.indexOf(":\\") == -1 
           && ( _path.indexOf("//") == -1 
             || _path.indexOf("//") > _path.indexOf("#")
             || _path.indexOf("//") > _path.indexOf("?")
