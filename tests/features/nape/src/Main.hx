3c05de008acaa054f64191c70b4665a818e70d50
underscorediscovery <sven@underscorediscovery.com>
2014-06-04 23:05:09 -0230

    Tests; nape - updated to be clean and to match latest physics code

--- tests/features/nape/src/Main.hx
+++ tests/features/nape/src/Main.hx
@@ -1,262 +1,34 @@
 
 import luxe.Input;
+import luxe.physics.nape.DebugDraw;
 
-import nape.constraint.Constraint;
-import nape.constraint.PivotJoint;
 import nape.geom.Vec2;
 import nape.phys.Body;
 import nape.phys.BodyType;
-import nape.phys.Compound;
-import nape.shape.Circle;
 import nape.shape.Polygon;
-import nape.shape.Shape;
-import nape.shape.ShapeType;
-import nape.space.Space;
-import phoenix.Color;
-import phoenix.geometry.Geometry;
-import phoenix.geometry.Vertex;
-import phoenix.Vector;
+import nape.shape.Circle;
+import nape.constraint.PivotJoint;
 
-
-class LuxeNapeDebug {
-
-    /**
-     * If true, then all bodies in the space (whether active or not) will be drawn.
-     * @default true
-     */
-    public var drawBodies:Bool=true;
-    /**
-     * If true, then things like the body centre of mass, and bounding box will be drawn.
-     * <br/><br/>
-     * This will only have an effect if drawBodies is true.
-     * @default false
-     */
-    public var drawBodyDetail:Bool=true;
-    /**
-     * If true, then things like shape centre of mass and bounding box will be drawn.
-     * <br/><br/>
-     * This will only have an effect if drawBodies is true.
-     * @default false
-     */
-    public var drawShapeDetail:Bool=false;
-    /**
-     * If true, then indicators of the shapes rotation will be drawn.
-     * <br/><br/>
-     * This will only have an effect if drawBodies is true.
-     * @default true
-     */
-    public var drawShapeAngleIndicators:Bool=true;
-    /**
-     * If true, then representations of the active constraints will be drawn.
-     * @default false
-     */
-    public var drawConstraints:Bool=false;
-
-    var geometry : Array<Geometry>;
-
-    public function new() {
-        geometry = [];
-    }
-
-    public function clear() {
-
-        for(g in geometry) {
-            geometry.remove(g);
-            if(!g.immediate) {
-                g.drop();
-            }
-            g = null;
-        }
-
-        geometry = [];
-
-    } //clear
-
-    function drawCircle( position:Vec2, radius:Float, color:Color, _immediate:Bool = true ) {
-        
-        var g = Luxe.draw.ring({
-            x:position.x,
-            y:position.y,
-            r:radius,
-            color : color,
-            immediate:_immediate
-        });
-
-        geometry.push( g );
-
-            //add a center point
-        g.vertices.insert( 0, new Vertex( new Vector(0, 0), color ) );
-        g.vertices.insert( 0, g.vertices[1].clone() );
-
-        
-    } //drawCircle
-
-    function drawPoint( _p:nape.geom.Vec2, color:Color, _immediate:Bool = true ) {
-        geometry.push(
-            Luxe.draw.ring({
-                x:_p.x,
-                y:_p.y,
-                r:2,
-                color : color,
-                immediate:_immediate
-            })
-        );
-    } //drawPoint
-
-    function drawAABB( _bounds:nape.geom.AABB, color:Color, _immediate:Bool = true ) {
-        geometry.push(
-            Luxe.draw.rectangle({
-                x:_bounds.x,
-                y:_bounds.y,
-                w:_bounds.width,
-                h:_bounds.height,
-                color : color,
-                immediate:_immediate
-            })
-        );
-    }
-
-    function drawPolygon( polygon:Polygon, color:Color, _immediate:Bool = true ) {
-        
-        var g = new Geometry();
-
-        g.primitive_type = phoenix.Batcher.PrimitiveType.lines;
-            
-        var i = 0;
-        for(v in polygon.worldVerts) {
-
-            g.add( new Vertex( new Vector(v.x, v.y), color ) );
-
-            if(i < polygon.worldVerts.length - 1) {
-                var vnext = polygon.worldVerts.at(i+1);
-                g.add( new Vertex( new Vector(vnext.x, vnext.y), color ));
-            }
-
-            i++;
-
-        }
-
-        var last = polygon.worldVerts.at(0);
-        g.add( new Vertex( new Vector(last.x, last.y), color ) );
-
-        if(g.vertices.length != 8) {
-            trace(g.vertices.length);
-        }
-
-        g.immediate = true;
-
-        geometry.push(g);
-
-        Luxe.addGeometry(g);        
-
-    } //drawPolygon
-
-
-    function draw_space( _space:Space, _immediate:Bool = true ) {
-        if(drawBodies) {
-            for(_b in _space.bodies) {
-                draw_body(_b, _immediate);                
-            }
-        }
-    } //space
-
-    function draw_compound( _space:Compound, _immediate:Bool = true ) {
-
-    }
-
-    function draw_body( _body:Body, _color:Int = 0xCC0000, _immediate:Bool = true ) {
-        
-        var _draw_color = new Color(1,1,1,1).rgb(_color);
-
-        if(_body.debugDraw) {
-            
-            for(_shape in _body.shapes) {                
-                draw_shape(_shape, _color, _immediate);
-            }
-            
-            if(drawBodyDetail) {
-                    //details are white for now
-                _draw_color.rgb(0xffffff);
-                    //always draw lesser details less visible
-                _draw_color.a = 0.1;
-                    //center point
-                drawPoint(_body.position, _draw_color, _immediate);
-                    //bounds
-                drawAABB(_body.bounds, _draw_color, _immediate);
-            }
-
-        } //_body.debugDraw
-
-    } //draw_body
-
-    function draw_shape( _shape:Shape, _color:Int = 0xCC0000, _immediate:Bool = true ) {
-
-        var _draw_color = new Color(1,1,1,1).rgb(_color);
-
-        if(_shape.body.isSleeping) {
-            _draw_color.a = 0.3;
-        }
-
-            //CIRCLE
-        if(_shape.isCircle()) {
-            var circle = _shape.castCircle;
-            drawCircle( circle.body.position, circle.radius, _draw_color, _immediate );
-            geometry[geometry.length-1].transform.rotation.setFromEuler(new Vector(0,0,_shape.body.rotation));
-        } else {
-            //POLYGON
-            var poly = _shape.castPolygon;
-            drawPolygon( poly, _draw_color, _immediate );
-        }
-
-    }
-
-    function draw_constraint( _constraint:Constraint, _immediate:Bool = true ) {
-
-    }
-
-
-    public function draw(object:Dynamic) {
-        
-        if(Std.is(object, Space)) {
-            draw_space( cast object );
-        } else 
-        
-        if(Std.is(object, Compound)) {
-            draw_compound( cast object );
-        } else
-        
-        if(Std.is(object, Body)) {
-            draw_body( cast object );
-        } else 
-
-        if(Std.is(object, Shape)) {
-            draw_shape( cast object );
-        } else 
-
-        if(Std.is(object, Constraint)) {
-            draw_constraint( cast object );
-        } 
-
-    } //draw
-
-}
 
 class Main extends luxe.Game {
 
-
-    var debug : LuxeNapeDebug;
+        //The ball body, so we can access it for key events
     var ball : Body;
+        //for attaching to the mouse when dragging
     var mouseJoint : PivotJoint;
 
-    public function ready() {
+        //the impulse to apply when pressing arrows
+    var impulse = 900;
 
-        debug = new LuxeNapeDebug();
+
+    public function ready() {
 
         reset_world();
 
     } //ready
 
     function reset_world() {
+
         var w = Luxe.screen.w;
         var h = Luxe.screen.h;
 
@@ -266,6 +38,7 @@ class Main extends luxe.Game {
         mouseJoint.stiff = false;
 
         var border = new Body(BodyType.STATIC);
+
             border.shapes.add(new Polygon(Polygon.rect(0, 0, w, -1)));
             border.shapes.add(new Polygon(Polygon.rect(0, h, w, 1)));
             border.shapes.add(new Polygon(Polygon.rect(0, 0, -1, h)));
@@ -273,22 +46,29 @@ class Main extends luxe.Game {
             border.space = Luxe.physics.nape.space;
 
             for (i in 0...16) {
+
                 var box = new Body(BodyType.DYNAMIC);
-                box.shapes.add(new Polygon(Polygon.box(16, 32)));
-                box.position.setxy((w / 2), ((h - 50) - 32 * (i + 0.5)));
-                box.space = Luxe.physics.nape.space;
-            }
+
+                    box.shapes.add(new Polygon(Polygon.box(16, 32)));
+                    box.position.setxy((w / 2), ((h - 50) - 32 * (i + 0.5)));
+                    box.space = Luxe.physics.nape.space;
+            
+            } //for 
 
             ball = new Body(BodyType.DYNAMIC);
-                ball.shapes.add(new Circle(50));
-                ball.position.setxy(50, h / 2);
-                ball.angularVel = 10;
-                ball.space = Luxe.physics.nape.space;
-    }
+
+            ball.shapes.add(new Circle(50));
+            ball.position.setxy(50, h / 2);
+            ball.angularVel = 10;
+            ball.space = Luxe.physics.nape.space;
+
+    } //reset_world
 
     function onmouseup( e:MouseEvent ) {
+        
         mouseJoint.active = false;
-    }
+
+    } //onmouseup
 
     function onmousedown( e:MouseEvent ) {
 
@@ -319,7 +99,8 @@ class Main extends luxe.Game {
         }
 
         mousePoint.dispose();
-    }
+
+    } //onmousedown
 
     function onmousemove( e:MouseEvent ) {
         if (mouseJoint.active) {
@@ -328,14 +109,14 @@ class Main extends luxe.Game {
     }
     
     function ontouchmove( e:MouseEvent ) {
+        
         if (mouseJoint.active) {
             mouseJoint.anchor1.setxy(e.pos.x, e.pos.y);
         }
-    }
+
+    } //ontouchmove
 
     public function onkeyup( e:KeyEvent ) {
-
-        var imp = 900;
 
         if(e.key == KeyValue.key_R) {
             Luxe.physics.nape.space.clear();
@@ -343,13 +124,13 @@ class Main extends luxe.Game {
         }
 
         if(e.key == KeyValue.left) {
-            ball.applyImpulse(new Vec2(-imp, 0), ball.position);
+            ball.applyImpulse(new Vec2(-impulse, 0), ball.position);
         }
         if(e.key == KeyValue.up) {
-            ball.applyImpulse(new Vec2(0, -imp), ball.position);
+            ball.applyImpulse(new Vec2(0, -impulse), ball.position);
         }
         if(e.key == KeyValue.right) {
-            ball.applyImpulse(new Vec2(imp, 0), ball.position);
+            ball.applyImpulse(new Vec2(impulse, 0), ball.position);
         }
 
         if(e.key == KeyValue.escape) {
@@ -359,8 +140,7 @@ class Main extends luxe.Game {
     } //onkeyup
 
     public function update(dt:Float) {
-        debug.clear();
-        debug.draw(Luxe.physics.nape.space);
+
     } //update
 
 
