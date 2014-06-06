90528e469491944ab6437063ee60d126119037c3
underscorediscovery <sven@underscorediscovery.com>
2014-06-06 18:21:57 -0230

    Html5; fixing macro causing build failure

--- luxe/BuildVersion.hx
+++ luxe/BuildVersion.hx
@@ -5,8 +5,11 @@ import haxe.macro.Context;
 
 import haxe.io.Bytes;
 import haxe.io.Path;
+
+#if luxe_native
 import sys.io.File;
 import sys.FileSystem;
+#end
 
 class BuildVersion {
 
@@ -14,32 +17,44 @@ class BuildVersion {
 		//which contains the .git repo, and build file directly
 	macro public static function latest() {
 
-		var location : String = Context.getPosInfos(Context.currentPos()).file;
-		var root : String = Path.addTrailingSlash(Path.directory(location));
-		var out : String = Path.join([root,'build']);
+		#if luxe_native
 
-		var build : String = try_git( root );
+			var location : String = Context.getPosInfos(Context.currentPos()).file;
+			var root : String = Path.addTrailingSlash(Path.directory(location));
+			var out : String = Path.join([root,'build']);
 
-		if(build != '') {
-				//the + is for semantic versioning
-			build = '+' + build.substr(0,10);
-			File.saveContent(out, build);
-			Context.addResource('build', Bytes.ofString(build));
-		}
+			var build : String = try_git( root );
 
-		return Context.makeExpr(build, Context.currentPos());
+			if(build != '') {
+					//the + is for semantic versioning
+				build = '+' + build.substr(0,10);
+				File.saveContent(out, build);
+				Context.addResource('build', Bytes.ofString(build));
+			}
+
+			return Context.makeExpr(build, Context.currentPos());
+
+		#else 
+
+			return macro null;
+
+		#end
 
 	} //latest
 
 	static function try_git(root:String) {
 
-		var git_path : String = Path.join([root,'.git/']);
-			git_path = Path.normalize(git_path);
+		#if luxe_native
 
-		if(FileSystem.exists(git_path) && FileSystem.isDirectory(git_path)) {
-			var ref_file = Path.normalize(Path.join([git_path,'refs/heads/master']));
-			return File.getContent(ref_file);
-		}
+			var git_path : String = Path.join([root,'.git/']);
+				git_path = Path.normalize(git_path);
+		
+			if(FileSystem.exists(git_path) && FileSystem.isDirectory(git_path)) {
+				var ref_file = Path.normalize(Path.join([git_path,'refs/heads/master']));
+				return File.getContent(ref_file);
+			}
+			
+		#end
 
 		return '';
 
