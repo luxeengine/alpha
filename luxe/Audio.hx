2f7b5a324d28cffa8d6db72a2756b0a1816f0521
underscorediscovery <sven@underscorediscovery.com>
2014-07-25 00:16:54 -0230

    Audio; reenable audio and mirror snow api

--- luxe/Audio.hx
+++ luxe/Audio.hx
@@ -23,111 +23,112 @@ class Audio {
     } //destroy
 
         /**
-            Create a named audio reference, with optional stream flag
-            **name** The name to assign this audio reference
-            **file** The asset file id from which the audio is loaded/streamed
-            **stream** Whether or not to stream the audio, default `false`
+            Create a named audio reference, with optional stream flag   
+            **name** The name to assign this audio reference   
+            **file** The asset file id from which the audio is loaded/streamed   
+            **stream** Whether or not to stream the audio, default `false`   
             **returns** The `Sound` instance, but the audio API stores this, use the API to manipulate it by name.
         */
-    public function create( _name:String, _file:String, ?_stream:Bool = false ) : Void {
-        // return core.lime.audio.create( _name, _file, _stream );
+    public function create( _name:String, _file:String, ?_stream:Bool = false ) : snow.audio.Sound {
+        return core.app.audio.create( _name, _file, _stream );
     } //create
 
         /**
-            Fetch a named audio reference
-            **name** The name to acquire audio reference
+            Fetch a named audio reference   
+            **name** The name to acquire audio reference   
             **returns** The `Sound` instance
         */
-    public function sound( _name:String ) : Void {
-        // return core.lime.audio.sound(_name);
-    } //sound
+    public function get( _name:String ) : snow.audio.Sound {
+        return core.app.audio.get(_name);
+    } //get
 
         /**
-            Determine whether a named audio reference exists
-            **name** The audio reference name to check
+            Determine whether a named audio reference exists   
+            **name** The audio reference name to check   
             **returns** true/false
         */
-    public function exists( _name:String ) : Void {
-        // return core.lime.audio.exists(_name);
+    public function exists( _name:String ) : Bool {
+        return get(_name) != null;
     } //exists
 
         /**
-            Determine whether a named audio reference is playing
-            **name** The audio reference name to check
-            **returns** true/false
-        */
-    public function playing( _name:String ) : Void {
-        // return core.lime.audio.playing( _name );
-    } //playing
-
-        /**
-            Set the on complete handler for a named audio reference
-            **name** The audio reference name to assign to
-            **handler** The callback to call when playback is complete
+            Set the on complete handler for a named audio reference   
+            **name** The audio reference name to assign to   
+            **handler** The callback to call when playback is complete   
             **returns** nothing
         */
     public function on_complete( _name:String, handler:Void->Void ) : Void {
-        // return core.lime.audio.sound( _name ).on_complete( handler );
+        // return get( _name ).on_complete( handler );
     } //playing
 
         /**
-            Loop a named audio reference indefinitely. Use `stop` if needed.
-            **name** The audio reference name to loop
+            Loop a named audio reference indefinitely. Use `stop` if needed.   
+            **name** The audio reference name to loop   
             **returns** nothing
         */
     public function loop( _name:String )  : Void{
-        // return core.lime.audio.loop(_name);
+        return core.app.audio.loop(_name);
     } //loop
 
         /**
-            Stop a named audio reference from playing (or looping)
-            **name** The audio reference name to stop
+            Stop a named audio reference from playing (or looping)   
+            **name** The audio reference name to stop   
             **returns** nothing
         */
     public function stop( _name:String ) : Void {
-        // return core.lime.audio.stop(_name);
+        return core.app.audio.stop(_name);
     } //stop
 
         /**
-            Play a named audio reference
-            **name** The audio reference name to play
-            **number_of_times** The exact number of times to play
-            **start_position_in_s** The start time, in seconds
+            Play a named audio reference   
+            **name** The audio reference name to play   
             **returns** nothing
         */
-    public function play( _name:String, ?_number_of_times:Int=1, ?_start_position_in_s:Float = 0 ) {
-        // return core.lime.audio.play(_name,_number_of_times, _start_position_in_s * 1000.0 );
+    public function play( _name:String ) {
+        return core.app.audio.play( _name );
     } //play
 
         /**
-            get/set the volume of a named audio reference
-            **name** The audio reference name to adjust
-            **volume** A new volume value (set), or leave empty (get)
-            **returns** The volume of `name` IF volume is unspecified
+            Pause a named audio reference   
+            **name** The audio reference name to pause   
+            **returns** nothing
         */
-    public function volume( _name:String, ?_volume:Float = null ) : Void {
-        // return core.lime.audio.volume(_name, _volume);
+    public function pause( _name:String ) {
+        return core.app.audio.pause( _name );
+    } //pause
+
+
+        /** Toggle a sound instance by name, pausing the sound or resuming it */
+    public function toggle(_name:String) {
+        return core.app.audio.toggle(_name);
+    } //toggle
+
+        /**
+            set the volume of a named audio reference   
+            **name** The audio reference name to adjust   
+            **volume** A new volume value
+        */
+    public function volume( _name:String, _volume:Float ) : Void {
+        return core.app.audio.volume(_name, _volume);
     } //volume
 
         /**
-            get/set the pan of a named audio reference
-            **name** The audio reference name to adjust
-            **pan** A new pan value (set), or leave empty (get)
-            **returns** The pan of `name` IF pan is unspecified
+            set the pan of a named audio reference   
+            **name** The audio reference name to adjust   
+            **pan** A new pan value
         */
-    public function pan( _name:String, ?_pan:Float = null ) : Void  {
-        // return core.lime.audio.pan(_name,_pan);
+    public function pan( _name:String, _pan:Float ) : Void  {
+        return core.app.audio.pan(_name,_pan);
     } //pan
 
         /**
-            get/set the position of a named audio reference
-            **name** The audio reference name to adjust
-            **pos** A new position value (set), or leave empty (get)
-            **returns** The position of `name` IF position is unspecified
+            set the pitch of a named audio reference   
+            **name** The audio reference name to adjust   
+            **pitch** A new pitch value
         */
-    public function position( _name:String, ?_pos:Float = null ) : Void {
-        // return core.lime.audio.position( _name, _pos );
-    } //position
+    public function pitch( _name:String, _pitch:Float ) : Void {
+        return core.app.audio.pitch( _name, _pitch );
+    } //pitch
 
     @:noCompletion public function process() {
 
