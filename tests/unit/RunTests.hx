973fd6bea7732e0d8f5efc8f5d1ade2d70773b61
underscorediscovery <sven@underscorediscovery.com>
2014-07-27 22:37:44 -0230

    continued cleanup and migration of tests.

--- tests/unit/RunTests.hx
+++ tests/unit/RunTests.hx
@@ -2,13 +2,18 @@
 
 class RunTests extends luxe.Game {
 
+    override function config( config:luxe.AppConfig ) {
+
+        config.has_window = false;
+
+        return config;
+
+    } //config
+
 	override function ready() {
-			
+
 			//run tests
 		new transform.Transforms();
-
-			//quit immediately
-		Luxe.shutdown();
 
 	} //ready
 
