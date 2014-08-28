73fccadbff014d2179ebed77955610d9b2e2327b
underscorediscovery <sven@underscorediscovery.com>
2014-08-28 20:46:26 -0230

    remove projects folder, these are now in luxe-projects repo, ask me for access

--- projects/squads/old/ld22/src/game/groups.js
+++
@@ -1,8 +0,0 @@
- 			Game.implement('createGroups',  function() {
-			
-					this.groups = [];
-					levels.current.groups.each(function(group) {
-						editor.makegroup( group );
-					});
-
-			});
--- projects/squads/old/ld22/src/game/tilemaps.js
+++
@@ -1,6 +0,0 @@
-		
-	Game.implement('createTilemaps',  function() {
-				
-		game.tilemap = new TiledMap(levels.current.tilemappath, game.physics).create(game.groups.objects, 0.2, false);
-		
-	});
