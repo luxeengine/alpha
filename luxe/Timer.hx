9a0fb1252b0affb490a28fac0d4346b6ecaacf07
underscorediscovery <sven@underscorediscovery.com>
2014-09-12 01:42:28 -0230

    Timer; add reset() to kill all existing schedules

--- luxe/Timer.hx
+++ luxe/Timer.hx
@@ -8,23 +8,42 @@ class Timer {
 
 
     @:noCompletion public var core : Core;
+    @:noCompletion public var timers : Array<snow.utils.Timer>;
 
 
     @:noCompletion public function new( _core:Core ) {
         core = _core;
+        timers = [];
     } //new
 
     @:noCompletion public function init() {
+
         _debug('\t timer initialized.');
+
     } //init
 
     @:noCompletion public function destroy() {
+
         _debug('\t timer shut down.');
+
     } //destroy
 
     @:noCompletion public function process() {
 
     } //process
+
+        /** Kill all existing scheduled timers created through `schedule` */
+    public function reset() {
+
+        for( t in timers ) {
+            t.stop();
+            t = null;
+        }
+
+        timers = null;
+        timers = [];
+
+    } //reset
 
     public function schedule( _time_in_seconds:Float, _on_time:Void->Void, ?repeat:Bool = false ) : snow.utils.Timer {
 
@@ -33,9 +52,12 @@ class Timer {
         t.run = function () {
             if(!repeat) {
                 t.stop ();
+                timers.remove(t);
             }
             _on_time();
         };
+
+        timers.push(t);
 
         return t;
 
