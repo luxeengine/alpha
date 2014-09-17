10cdc99cfe445dcc134906ff1932a0b935dcbeb3
underscorediscovery <sven@underscorediscovery.com>
2014-09-17 14:49:50 -0230

    update omnibar and search boxes

--- luxe/style/js/omnibar.js
+++ luxe/style/js/omnibar.js
@@ -1,8 +1,17 @@
+
 $(document).ready(function() {
-    var relpath = '';
-    var omnibar_visible = false;
-    $('body').on('keyup', function(e) {
-        if(e.keyCode == 27) {
+
+        //hide by default
+    $('#omnibar').hide();
+
+        //if the list is not found, nothing happens
+    var list_node = $('#typelist');
+    if(list_node.length) {
+
+        var relpath = '';
+        var omnibar_visible = false;
+
+        var close_omnibar = function() {
             if(omnibar_visible) {
                 omnibar_visible = false;
                 $('#omnibar_text').val('');
@@ -10,56 +19,86 @@ $(document).ready(function() {
                 $('#omnibar').hide();
                 $('#omnibar_text').tooltipster('hide');
             }
-        }
-    });
-    $('body').on('keypress', function(e) {
-        if(!omnibar_visible) {
-            omnibar_visible = true;
-            $('#omnibar').show();
-        }
-        $('#omnibar_text').focus();
-        $('#omnibar_text').tooltipster('show');
-    });
-    $('#omnibar').hide();
-    var list_node = $('#typelist');
-    if(list_node.length) {
+        } //close_omnibar
+
+        var focus_omnibar = function() {
+
+            if(!omnibar_visible) {
+                omnibar_visible = true;
+                $('#omnibar').show();
+            }
+
+            $('#omnibar_text').focus();
+            $('#omnibar_text').tooltipster('show');
+
+        } //focus_omnibar
+
+            //on any keypress, show if hidden, or focus if showing
+        $('body').on('keypress', focus_omnibar);
+        $('#search_bar').on('click', focus_omnibar);
+        $('#omnibar_close').on('click', close_omnibar);
+
+            //list of types
         var list = list_node.attr('data-types');
-        relpath = list_node.attr('data-relpath');
             list = list.split(',');
-        var c = 'start typing...';
+            //fetch the relative path
+        relpath = list_node.attr('data-relpath');
+
+            //create the tooltip to display the types
         $('#omnibar_text').tooltipster({
+
             position : 'bottom',
             trigger : 'click',
             interactive:true,
             minWidth:500,
-            content:$('<span>' + c + '</span>'),
+            content:$('<span>start typing...</span>'),
             theme:'tooltipster-shadow'
-        });
 
+        }); //create tooltipster
+
+            //hide on escape
+        $('body').on('keyup', function(e) {
+
+            if(e.keyCode == 27) {
+                close_omnibar();
+            }
+
+        }); //keyup
+
+            //on text input
         $('#omnibar_text').on('input', function(e) {
+
             var val = $('#omnibar_text').val();
+
+                //if value typed in
             if(val) {
+
                 var results = list.filter(function(a){
                     var reg = new RegExp( val,'gi');
-                    if(reg.exec(a)) {
-                        return a;
-                    }
-                });
-                results = results.map(function(a){
-                    var _link = relpath + 'api/' + a.replace(/\./gi, '/') + '.html';
-                    var _type = a;
-                    a = '<a href="' + _link + '">' + _type + '</a>' ;
-                    return a;
-                });
+                    return reg.exec(a);
+                }); //filter by types
+
+                results = results.map(function(_type){
+                    var _link = relpath + 'api/' + _type.replace(/\./gi, '/') + '.html';
+                    _type = '<a href="' + _link + '">' + _type + '</a>' ;
+                    return _type;
+                }); //map to the link types
+
                 if(results.length) {
                     val = results.join('<br/>');
                 } else {
                     val = 'no results';
                 }
+
             } else {
                 val = 'start typing...';
             }
+
+                //set the content
             $('#omnibar_text').tooltipster('content', $( '<span>' + val + '</span>'));
-        });
-    }
-});
+
+        }); // input
+
+    } //if list node found
+
+}); //document ready
