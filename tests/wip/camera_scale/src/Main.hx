2151f3d9c55cda23148542c8430f9e6433a07762
underscorediscovery <sven@underscorediscovery.com>
2014-09-17 22:14:51 -0230

    Tests; camera scale; include zooming factor, and test size property

--- tests/wip/camera_scale/src/Main.hx
+++ tests/wip/camera_scale/src/Main.hx
@@ -5,11 +5,21 @@ import luxe.Sprite;
 import luxe.Vector;
 import luxe.Rectangle;
 import luxe.Text;
+import luxe.Camera;
 import phoenix.Texture;
 
 class Main extends luxe.Game {
 
     var text : Text;
+
+    override function config( config:luxe.AppConfig ) {
+
+        config.window.width = 720;
+        config.window.height = 420;
+
+        return config;
+
+    } //config
 
     override function ready() {
 
@@ -29,14 +39,13 @@ class Main extends luxe.Game {
                 centered: false
             });
 
-            Luxe.camera.zoom = 4;
-            Luxe.camera.center = new Vector(apartment.width/2, apartment.height/2);
+            Luxe.camera.size = new Vector(apartment.width, apartment.height);
 
         } //onload
 
         text = new Text({
             pos:new Vector(3,100),
-            text : 'move the mouse',
+            text : 'size mode : fit\nmove the mouse',
             color : new Color(0,0,0),
             size : 4
         });
@@ -44,14 +53,47 @@ class Main extends luxe.Game {
 
     } //ready
 
+    override function onmousewheel( e:MouseEvent ) {
+        if( e.y > 0 ) {
+            Luxe.camera.zoom += 0.01;
+        } else if( e.y < 0 ) {
+            Luxe.camera.zoom -= 0.01;
+        }
+    }
+
     override function onmousemove( e:MouseEvent ) {
-        var world_coord = Luxe.camera.screen_point_to_world(e.pos);
+        update_text(e.pos.int());
+    }
+
+    function update_text( ?pos:Vector ) {
+        if(pos == null) {
+            pos = Luxe.mouse.clone().int();
+        }
+
+        var world_coord = Luxe.camera.screen_point_to_world(pos);
+
         text.text =
-              'screen coord: ' + e.pos.int() + '\n'
+              'size mode: ' + Luxe.camera.size_mode + '\n'
+            + 'screen coord: ' + pos + '\n'
             + 'world coord: ' + world_coord.int();
     }
 
     override function onkeyup( e:KeyEvent ) {
+
+            //cycle sizing mode by space bar
+        if(e.keycode == Key.space) {
+            switch(Luxe.camera.size_mode) {
+                case fit:
+                    Luxe.camera.size_mode = SizeMode.cover;
+                    update_text();
+                case cover:
+                    Luxe.camera.size_mode = SizeMode.contain;
+                    update_text();
+                case contain:
+                    Luxe.camera.size_mode = SizeMode.fit;
+                    update_text();
+            }
+        }
 
         if(e.keycode == Key.escape) {
             Luxe.shutdown();
