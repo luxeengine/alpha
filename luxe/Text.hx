7b7a51615d8318a9a4be9e44795330b699b8ef14
underscorediscovery <sven@underscorediscovery.com>
2014-11-19 00:55:42 -0330

    Text; update to use new TextGeometry

--- luxe/Text.hx
+++ luxe/Text.hx
@@ -8,117 +8,113 @@ import luxe.options.TextOptions;
 
 import phoenix.Batcher;
 import phoenix.BitmapFont;
-import phoenix.geometry.CompositeGeometry;
+import phoenix.geometry.TextGeometry;
 
 typedef TextAlign = phoenix.BitmapFont.TextAlign;
 
 class Text extends Visual {
 
-    public var font : BitmapFont;
-    var _batcher : Batcher;
+        public var geom : TextGeometry;
 
-    public var composite_geometry : CompositeGeometry;
+    //regular font stuff
 
-    @:isVar public var text     (default,set) : String = '';
-    @:isVar public var textsize (default,set) : Float = 32;
+        public var text (get,set) : String;
+            function get_text() return geom.text;
+            function set_text(_s) return geom.text = _s;
+        public var font (get,set) : BitmapFont;
+            function get_font() return geom.font;
+            function set_font(_f) return geom.font = _f;
+        public var point_size (get,set) : Float;
+            function get_point_size() return geom.point_size;
+            function set_point_size(_s) return geom.point_size = _s;
+        public var align (get,set) : TextAlign;
+            function get_align() return geom.align;
+            function set_align(_a) return geom.align = _a;
+        public var align_vertical (get,set) : TextAlign;
+            function get_align_vertical() return geom.align_vertical;
+            function set_align_vertical(_a) return geom.align_vertical = _a;
 
-    public var ready : Bool = false;
+    //distance field specific
+
+        public var sdf (get,set) : Bool;
+            function get_sdf() return geom.sdf;
+            function set_sdf(_s) return geom.sdf = _s;
+
+        public var smoothness (get,set) : Float;
+            function get_smoothness() return geom.smoothness;
+            function set_smoothness(_s) return geom.smoothness = _s;
+        public var thickness (get,set) : Float;
+            function get_thickness() return geom.thickness;
+            function set_thickness(_t) return geom.thickness = _t;
+
+        public var outline (get,set) : Float;
+            function get_outline() return geom.outline;
+            function set_outline(_o) return geom.outline = _o;
+        public var outline_color (get,set) : Color;
+            function get_outline_color() return geom.outline_color;
+            function set_outline_color(_c) return geom.outline_color = _c;
+
+        public var glow_threshold (get,set) : Float;
+            function get_glow_threshold() return geom.glow_threshold;
+            function set_glow_threshold(_s) return geom.glow_threshold = _s;
+        public var glow_amount (get,set) : Float;
+            function get_glow_amount() return geom.glow_amount;
+            function set_glow_amount(_e) return geom.glow_amount = _e;
+        public var glow_color (get,set) : Color;
+            function get_glow_color() return geom.glow_color;
+            function set_glow_color(_c) return geom.glow_color = _c;
+
     public var text_options : TextOptions;
 
     public function new( _options : TextOptions ) {
 
-        _batcher = (_options.batcher == null) ? Luxe.renderer.batcher : _options.batcher;
-
-            //these are handled here because of the Visual
-            //right now, WIP:#98, and because text_options is used later
-
-        if(_options.pos == null)     _options.pos = new Vector();
-        if(_options.color == null)   _options.color = new Color();
-        if(_options.depth == null)   _options.depth = 0;
-        if(_options.group == null)   _options.group = 0;
-        if(_options.visible == null) _options.visible = true;
-
+            //store for later
         text_options = _options;
 
-        var visual_opt : VisualOptions = {
-            name : _options.name,
-            no_scene : _options.no_scene,
-            batcher : _batcher,
-            color : _options.color,
-            parent : _options.parent,
-            visible : _options.visible,
-            // pos : _options.pos,
-            depth : _options.depth,
-            group : _options.group,
-            no_geometry : true
-        };
+        var _batcher = (_options.batcher == null) ? Luxe.renderer.batcher : _options.batcher;
 
-        super(visual_opt);
+            //create the text geometry
+        geom = new TextGeometry({
 
-    //font
-        var _font : Dynamic = (_options.font == null) ? null : _options.font;
+            text: _options.text,
+            font: _options.font,
+            point_size: _options.point_size,
 
-        if(_font == null) _font = Luxe.renderer.font;
+            bounds : _options.bounds,
+            align : _options.align,
+            align_vertical : _options.align_vertical,
 
-        textsize = (_options.size == null) ? 24 : _options.size;
+            sdf: _options.sdf,
+            smoothness: _options.smoothness,
+            thickness: _options.thickness,
 
-        if(Std.is(_font, String)) {
+            outline: _options.outline,
+            outline_color: _options.outline_color,
 
-                //fetch font path directly from the same path
-            var _folder = haxe.io.Path.directory(_font) + '/';
-                //create the font from the path + file name
-            font = Luxe.loadFont( _font, _folder, onloaded );
+            glow_threshold: _options.glow_threshold,
+            glow_amount: _options.glow_amount,
+            glow_color: _options.glow_color,
+            batcher: _batcher
 
-        } else {
+        });
 
-                //supplied a precreated font?
-            font = _font;
-            ready = true;
 
-        }
+        _options.geometry = geom;
+        _options.shader = geom.shader;
 
-        text_options.size = textsize;
+            //create the visual
+        super(_options);
 
-            //Apply the setter, which will draw the geometry
-        text = _options.text;
+        // trace(shader);
+        // trace(geom.shader);
 
     } //new
 
-    var size_rect_cache : Rectangle;
-    var dim_cache : Vector;
-
     public function point_inside( p:Vector ) {
 
-        if(font == null) {
-            return false;
-        }
-
-        if(!ready) {
-            return false;
-        }
-
-        if(size_rect_cache == null) {
-            dim_cache = new Vector();
-            size_rect_cache = new Rectangle();
-        }
-
-        font.dimensions_of(text, text_options.size, dim_cache);
-
-            size_rect_cache.x = pos.x-(dim_cache.x / 2);
-            size_rect_cache.y = pos.y;
-            size_rect_cache.w = dim_cache.x;
-            size_rect_cache.h = dim_cache.y;
-
-        return size_rect_cache.point_inside(p);
+        return false;
 
     } //point_inside
-
-    @:noCompletion public function onloaded( font:BitmapFont ) {
-
-        ready = true;
-        text = text + '';
-
-    } //onloaded
 
     override function set_pos_from_transform( _p:Vector ) {
 
@@ -126,95 +122,5 @@ class Text extends Visual {
         text_options.pos = pos;
 
     } //set_pos_from_transform
-
-
-    function set_textsize(v:Float) : Float {
-
-        if(text_options != null) {
-            textsize = v;
-            text_options.size = textsize;
-            text = text+'';
-        }
-
-        return textsize;
-
-    } //set_textsize
-
-    override function set_color(c:Color) : Color {
-
-        super.set_color(c);
-
-        text_options.color = color;
-
-        return color;
-
-    } //set_color
-
-    override function set_visible(b:Bool) : Bool {
-
-        super.set_visible(b);
-
-        text_options.visible = visible;
-
-        return visible;
-
-    } //set_visible
-
-
-    override function set_locked(l:Bool) : Bool {
-
-        super.set_locked(l);
-
-        text_options.locked = locked;
-
-        return locked;
-
-    } //set_locked
-
-    function set_text( t:String ) {
-
-        text = t;
-
-        if(ready && text_options != null) {
-
-            text_options.text = t;
-
-                //clean up first
-            if(geometry != null) {
-                geometry.drop();
-            }
-
-                //make sure its cleared
-            geometry = null;
-            composite_geometry = null;
-
-                //now recreate the new text
-            composite_geometry = font.draw_text(text_options);
-
-                //now we apply the new info to the visual first
-            origin  = composite_geometry.transform.origin.clone();
-            pos     = composite_geometry.transform.pos.clone();
-            locked  = composite_geometry.locked;
-            visible = composite_geometry.visible;
-
-                // since the transform of the visual manages the position, origin
-                // the geometry cannot have it as well, this would apply it twice
-            composite_geometry.transform.origin = new Vector();
-            composite_geometry.transform.pos = new Vector();
-
-                //and then assign it
-            ignore_texture_on_geometry_change = true;
-
-                geometry = composite_geometry;
-
-            ignore_texture_on_geometry_change = false;
-
-        }
-
-        return text;
-
-    } //set_text
-
-
 
 } //Text
