ee4ab22e147cadc10d22523de60d782a77b34227
underscorediscovery <sven@underscorediscovery.com>
2014-11-23 10:26:04 -0330

    Utils; text wrap column should handle unicode strings (best it can) and capture all potential groups

--- luxe/utils/Utils.hx
+++ luxe/utils/Utils.hx
@@ -176,7 +176,7 @@ class Utils {
             //take note that the ${_column} is string interpolation, not part of the regex.
             //i.e (.{1,80})( +|$)\n?|(.{80})
 
-        return new EReg('(.{1,${_column}})( +|$)\n?|(.{${_column}})','gim').replace(_text, '$1${_brk}');
+        return new EReg('(.{1,${_column}})(?: +|$)\n?|(.{${_column}})','gimu').replace(_text, '$1$2${_brk}');
 
     } //text_wrap_column
 
