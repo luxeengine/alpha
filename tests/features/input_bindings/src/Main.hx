1ced5b13b7a5433ec5fa0ad0b095a40714c212c9
underscorediscovery <sven@underscorediscovery.com>
2014-11-25 14:49:36 -0330

    Input; fix named input bindings for mouse not being stored for pressed/released immediate query and added more test case to the test. Thanks @Absurditure for reporting.

--- tests/features/input_bindings/src/Main.hx
+++ tests/features/input_bindings/src/Main.hx
@@ -6,6 +6,10 @@ class Main extends luxe.Game {
 
     override function ready() {
 
+        Luxe.showConsole(true);
+
+        trace('press space, z, or mouse left.');
+
         Luxe.input.add('fire', Key.space);
         Luxe.input.add('fire', Key.key_z);
         Luxe.input.add('fire', MouseButton.left);
@@ -25,6 +29,26 @@ class Main extends luxe.Game {
             Luxe.shutdown();
         }
     } //onkeyup
+
+    var next_trace:Float = 0.0;
+    override function update(dt:Float) {
+
+        if(Luxe.input.inputdown('fire')) {
+            if(Luxe.time > next_trace) {
+                trace('fire is held down');
+                next_trace = Luxe.time + 0.4;
+            }
+        }
+
+        if(Luxe.input.inputpressed('fire')) {
+            trace('fire pressed in update');
+        }
+
+        if(Luxe.input.inputreleased('fire')) {
+            trace('fire released in update');
+        }
+
+    }
 
 
 } //Main
