dfa4460b48b9b80e6e678aacab85f7c6ee9b8de8
KeyMaster <AccessKeyMaster@gmx.net>
2014-11-28 19:34:10 +0000

    Point moving and control points display

--- tests/wip/splines/src/Main.hx
+++ tests/wip/splines/src/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import luxe.Color;
 import luxe.Vector;
 import luxe.Input;
 import phoenix.geometry.LineGeometry;
@@ -8,6 +9,9 @@ class Main extends luxe.Game {
 	var y:Float = 200;
 	var x:Float = 100;
 	var geom:SplineGeometry;
+	
+	var pointNum:Int = 0;
+	var c:Color = new Color().rgb(0x5E6CF0);
 	override function ready() {
 		geom = new SplineGeometry( {
 			p0:new Vector(0, 100, 0),
@@ -29,42 +33,53 @@ class Main extends luxe.Game {
 				geom.lineWidth += 0.5;
 			case Key.key_g:
 				geom.lineWidth -= 0.5;
+			case Key.left:
+				pointNum--;
+				if (pointNum < 0) pointNum = 0;
+			case Key.right:
+				pointNum++;
+				if (pointNum > 3) pointNum = 3;
 		}
 	}
 
 	override function update(dt:Float) {
-		if (Luxe.input.keydown(Key.key_w)) {
-			y -= 200 * dt;
-			geom.set( {
-				p0:new Vector(0, 100, 0),
-				p1:new Vector(x, y, 0),
-				p2:new Vector(200, 0, 0),
-				p3:new Vector(300, 100, 0)
-			});
+		if (Luxe.input.keydown(Key.key_a) || Luxe.input.keydown(Key.key_d) || Luxe.input.keydown(Key.key_w) || Luxe.input.keydown(Key.key_s)) {
+			var v:Vector = new Vector();
+			if (Luxe.input.keydown(Key.key_w)) {
+				v.y -= 200 * dt;
+				
+			}
+			else if (Luxe.input.keydown(Key.key_s)) {
+				v.y += 200 * dt;
+			}
 			
-		}
-		else if (Luxe.input.keydown(Key.key_s)) {
-			y += 200 * dt;
-			geom.set( {
-				p0:new Vector(0, 100, 0),
-				p1:new Vector(x, y, 0),
-				p2:new Vector(200, 0, 0),
-				p3:new Vector(300, 100, 0)
-			});
-		}
-		
-		if (Luxe.input.keydown(Key.key_d)) {
-			x += 200 * dt;
-			geom.set( {
-				p0:new Vector(0, 100, 0),
-				p1:new Vector(x, y, 0),
-				p2:new Vector(200, 0, 0),
-				p3:new Vector(300, 100, 0)
-			});
-		}
-		else if (Luxe.input.keydown(Key.key_a)) {
-			x -= 200 * dt;
-			geom.p1.x = x;
+			if (Luxe.input.keydown(Key.key_d)) {
+				v.x += 200 * dt;
+			}
+			else if (Luxe.input.keydown(Key.key_a)) {
+				v.x -= 200 * dt;
+			}
+			var point:Vector;
+			if (pointNum == 0) {
+				point = geom.p0;
+				point.add(v);
+				geom.p0 = point;
+			}
+			else if (pointNum == 1) {
+				point = geom.p1;
+				point.add(v);
+				geom.p1 = point;
+			}
+			else if (pointNum == 2) {
+				point = geom.p2;
+				point.add(v);
+				geom.p2 = point;
+			}
+			else if (pointNum == 3) {
+				point = geom.p3;
+				point.add(v);
+				geom.p3 = point;
+			}
 		}
 		
 		if (Luxe.input.keydown(Key.up)) {
@@ -85,6 +100,34 @@ class Main extends luxe.Game {
 			trace(geom.MinPoints);
 		}
 		
+		Luxe.draw.circle( {
+			r:4,
+			x:geom.p0.x,
+			y:geom.p0.y,
+			color:c,
+			immediate:true
+		});
+		Luxe.draw.circle( {
+			r:4,
+			x:geom.p1.x,
+			y:geom.p1.y,
+			color:c,
+			immediate:true
+		});
+		Luxe.draw.circle( {
+			r:4,
+			x:geom.p2.x,
+			y:geom.p2.y,
+			color:c,
+			immediate:true
+		});
+		Luxe.draw.circle( {
+			r:4,
+			x:geom.p3.x,
+			y:geom.p3.y,
+			color:c,
+			immediate:true
+		});
 		
 	}
 }
