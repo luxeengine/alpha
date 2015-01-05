3430a546f35e09a81f97f7810a944c13d9a3fd7f
Anders Nissen <andnis@gmail.com>
2015-01-05 21:23:07 +0100

    Fix indentation

--- luxe/NineSlice.hx
+++ luxe/NineSlice.hx
@@ -120,88 +120,88 @@ class NineSlice extends luxe.Visual {
         midwidth = Math.abs(width - left - right);
         midheight = Math.abs(height - top - bottom);
 
-            //top left
-            slices[0].source_width  = left;
-            slices[0].source_height = top;
-            slices[0].source_x = source_x;
-            slices[0].source_y = source_y;
-            slices[0].pos.set_xy(0,0);
-            slices[0].width = left;
-            slices[0].height = top;
+        //top left
+        slices[0].source_width  = left;
+        slices[0].source_height = top;
+        slices[0].source_x = source_x;
+        slices[0].source_y = source_y;
+        slices[0].pos.set_xy(0,0);
+        slices[0].width = left;
+        slices[0].height = top;
 
-            //top middle
-            slices[1].source_width  = source_w - left - right;
-            slices[1].source_height = top;
-            slices[1].source_x = source_x+left;
-            slices[1].source_y = source_y;
-            slices[1].pos.set_xy(left,0);
-            slices[1].width = width - left - right;
-            slices[1].height = top;
+        //top middle
+        slices[1].source_width  = source_w - left - right;
+        slices[1].source_height = top;
+        slices[1].source_x = source_x+left;
+        slices[1].source_y = source_y;
+        slices[1].pos.set_xy(left,0);
+        slices[1].width = width - left - right;
+        slices[1].height = top;
 
-            //top right
-            slices[2].source_width  = right;
-            slices[2].source_height = top;
-            slices[2].source_x = source_x+ (source_w - right);
-            slices[2].source_y = source_y;
-            slices[2].pos.set_xy(left + midwidth,0);
-            slices[2].width = right;
-            slices[2].height = top;
-
-
-            //middle left
-            slices[3].source_width  = left;
-            slices[3].source_height = source_h - top - bottom;
-            slices[3].source_x = source_x;
-            slices[3].source_y = source_y+top;
-            slices[3].pos.set_xy(0,top);
-            slices[3].width = left;
-            slices[3].height = height - top - bottom;
-
-            //middle middle
-            slices[4].source_width  = source_w - left - right;
-            slices[4].source_height = source_h - top - bottom;
-            slices[4].source_x = source_x+left;
-            slices[4].source_y = source_y+top;
-            slices[4].pos.set_xy(left,top);
-            slices[4].width = width - left - right;
-            slices[4].height = height - top - bottom;
-
-            //middle right
-            slices[5].source_width  = right;
-            slices[5].source_height = source_h - top - bottom;
-            slices[5].source_x = source_x+(source_w - right);
-            slices[5].source_y = source_y+top;
-            slices[5].pos.set_xy(left + midwidth,top);
-            slices[5].width = right;
-            slices[5].height = height - top - bottom;
+        //top right
+        slices[2].source_width  = right;
+        slices[2].source_height = top;
+        slices[2].source_x = source_x+ (source_w - right);
+        slices[2].source_y = source_y;
+        slices[2].pos.set_xy(left + midwidth,0);
+        slices[2].width = right;
+        slices[2].height = top;
 
 
-            //bottom left
-            slices[6].source_width  = left;
-            slices[6].source_height = bottom;
-            slices[6].source_x = source_x;
-            slices[6].source_y = source_y+ (source_h - bottom);
-            slices[6].pos.set_xy(0,top + midheight);
-            slices[6].width = left;
-            slices[6].height = bottom;
+        //middle left
+        slices[3].source_width  = left;
+        slices[3].source_height = source_h - top - bottom;
+        slices[3].source_x = source_x;
+        slices[3].source_y = source_y+top;
+        slices[3].pos.set_xy(0,top);
+        slices[3].width = left;
+        slices[3].height = height - top - bottom;
 
-            //bottom middle
-            slices[7].source_width  = source_w - left - right;
-            slices[7].source_height = bottom;
-            slices[7].source_x = source_x+left;
-            slices[7].source_y = source_y+(source_h - bottom);
-            slices[7].pos.set_xy(left,top + midheight);
-            slices[7].width = width - left - right;
-            slices[7].height = bottom;
+        //middle middle
+        slices[4].source_width  = source_w - left - right;
+        slices[4].source_height = source_h - top - bottom;
+        slices[4].source_x = source_x+left;
+        slices[4].source_y = source_y+top;
+        slices[4].pos.set_xy(left,top);
+        slices[4].width = width - left - right;
+        slices[4].height = height - top - bottom;
 
-            //bottom right
-            slices[8].source_width  = right;
-            slices[8].source_height = bottom;
-            slices[8].source_x = source_x+(source_w - right);
-            slices[8].source_y = source_y+(source_h - bottom);
-            slices[8].pos.set_xy(left + midwidth, top + midheight);
-            slices[8].width = right;
-            slices[8].height = bottom;
+        //middle right
+        slices[5].source_width  = right;
+        slices[5].source_height = source_h - top - bottom;
+        slices[5].source_x = source_x+(source_w - right);
+        slices[5].source_y = source_y+top;
+        slices[5].pos.set_xy(left + midwidth,top);
+        slices[5].width = right;
+        slices[5].height = height - top - bottom;
+
+
+        //bottom left
+        slices[6].source_width  = left;
+        slices[6].source_height = bottom;
+        slices[6].source_x = source_x;
+        slices[6].source_y = source_y+ (source_h - bottom);
+        slices[6].pos.set_xy(0,top + midheight);
+        slices[6].width = left;
+        slices[6].height = bottom;
+
+        //bottom middle
+        slices[7].source_width  = source_w - left - right;
+        slices[7].source_height = bottom;
+        slices[7].source_x = source_x+left;
+        slices[7].source_y = source_y+(source_h - bottom);
+        slices[7].pos.set_xy(left,top + midheight);
+        slices[7].width = width - left - right;
+        slices[7].height = bottom;
+
+        //bottom right
+        slices[8].source_width  = right;
+        slices[8].source_height = bottom;
+        slices[8].source_x = source_x+(source_w - right);
+        slices[8].source_y = source_y+(source_h - bottom);
+        slices[8].pos.set_xy(left + midwidth, top + midheight);
+        slices[8].width = right;
+        slices[8].height = bottom;
 
     } //update_size
 
@@ -220,6 +220,7 @@ class NineSlice extends luxe.Visual {
         midwidth = Math.abs(width - left - right);
         midheight = Math.abs(height - top - bottom);
 
+            //top left
         slices.push({
             source_width  : left,
             source_height : top,
