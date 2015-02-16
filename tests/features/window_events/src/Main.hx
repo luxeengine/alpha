38e00fdaf546ab26a6e78204869f20594e2bd1fb
underscorediscovery <sven@underscorediscovery.com>
2015-02-16 15:14:06 -0330

    Tests; window_event; add resize on R key and adjust viewport and show screen resized region

--- tests/features/window_events/src/Main.hx
+++ tests/features/window_events/src/Main.hx
@@ -1,10 +1,16 @@
 
 import luxe.Input;
 import luxe.Screen;
+import luxe.Vector;
 
 class Main extends luxe.Game {
 
+    var start : Vector;
+
     override function ready() {
+
+        Luxe.renderer.clear_color.rgb(0x3e8fb1);
+        start = Luxe.screen.size;
 
     } //ready
 
@@ -14,7 +20,23 @@ class Main extends luxe.Game {
             Luxe.shutdown();
         }
 
+        if(e.keycode == Key.key_r) {
+            Luxe.core.app.window.set_size(
+                    Luxe.utils.random.int(start.x - 50, start.x + 100 ),
+                    Luxe.utils.random.int(start.y - 50, start.y + 100 )
+            );
+        }
+
     } //onkeyup
+
+    override function onrender() {
+        Luxe.draw.rectangle({
+            x:20, y:20,
+            w:Luxe.screen.w - 40,
+            h:Luxe.screen.h - 40,
+            immediate:true
+        });
+    }
 
     override function onwindowmoved( e:WindowEvent ) {
         trace('window moved : ${e.event.x} / ${e.event.y}');
@@ -24,6 +46,7 @@ class Main extends luxe.Game {
     }
     override function onwindowsized( e:WindowEvent ) {
         trace('window sized : ${e.event.x} / ${e.event.y}');
+        Luxe.camera.viewport = new luxe.Rectangle(0, 0, e.event.x, e.event.y);
     }
     override function onwindowminimized( e:WindowEvent ) {
         trace('window minimized');
