4fd0f55d2551514d9ae22942ceee4b18e6538cb8
KeyMaster <AccessKeyMaster@gmx.net>
2015-02-23 17:17:23 +0000

    New ray class, has all the functionality of the old one, with a bunch of extra stuff. Includes API changes.

--- phoenix/Ray.hx
+++ phoenix/Ray.hx
@@ -4,45 +4,58 @@ import phoenix.Rectangle;
 import phoenix.Vector;
 import phoenix.Camera;
 
-class Ray { 
-
-    public var origin : Vector;
-    public var end : Vector;
-    public var dir : Vector;
-
-    var camera : Camera;
-    var viewport : Rectangle;
-
-        //optional viewport size
-    public function new( _screen_pos:Vector, _camera:Camera, ?_viewport:Rectangle=null ) {
-
-        if(_viewport == null) {
-            _viewport = new Rectangle(0,0,Luxe.screen.w, Luxe.screen.h);
-        }
-
-        if(_camera == null) throw "Camera required for a ray!";
-
-        camera = _camera;
-        viewport = _viewport;
-
-       refresh(_screen_pos);
-
-    } //new
-
-    public function refresh( _screen_pos:Vector ) {
-
-            //calculate the x and y in normalized device coordinates first [-1, 1] on both axis
-        var ndc_x : Float = (_screen_pos.x/viewport.w  - 0.5) * 2.0;
-        var ndc_y : Float = ((viewport.h - _screen_pos.y)/viewport.h - 0.5) * 2.0;
-
-            //To calculate a ray we use [0,1] for the depth so we can get a direction
-        var start_ndc : Vector = new Vector( ndc_x, ndc_y, 0.0, 1.0 );
-        var end_ndc : Vector = new Vector( ndc_x, ndc_y, 1.0, 1.0 );
-            //So we have a near and a far clipping space coords for the mouse, now unproject it using the camera
-        origin = camera.unproject(start_ndc);
-        end = camera.unproject(end_ndc);
-        dir = Vector.Subtract(end, origin);
-
-    } //refresh
-
-} //Ray
+class Ray {
+	public var screen_pos(default, set) : Vector;
+	var start_ndc : Vector;
+	var end_ndc : Vector;
+	public var origin : Vector;
+	public var end : Vector;
+	public var dir : Vector;
+	
+	var camera : Camera;
+	var viewport : Rectangle;
+	
+	public function new(_screen_pos:Vector, _camera:Camera, ?_viewport:Rectangle = null) {
+		if (_camera == null) {
+			throw "Camera required for a ray!";
+		}
+		
+		if (_viewport == null) {
+			_viewport = new Rectangle(0, 0, Luxe.screen.w, Luxe.screen.h);
+		}
+		
+		camera = _camera;
+		viewport = _viewport;
+		start_ndc = new Vector(0,0,0,1);
+		end_ndc = new Vector(0, 0, 1, 1);
+		screen_pos = _screen_pos;
+	}
+	
+	function set_screen_pos(_v:Vector):Vector {
+		start_ndc.x = end_ndc.x = (_v.x/viewport.w - 0.5) * 2;
+		start_ndc.y = end_ndc.y = ((viewport.h - _v.y) / viewport.h - 0.5) * 2;
+		refresh();
+		return screen_pos = _v;
+	}
+	
+	public function refresh():Void {
+		origin = camera.unproject(start_ndc);
+		end = camera.unproject(end_ndc);
+		dir = Vector.Subtract(end, origin);
+	}
+	
+	public function get_point(along:Float):Vector {
+		dir.length = along;
+		return Vector.Add(origin, dir);
+	}
+	
+	public function refresh_and_get(along:Float):Vector {
+		refresh();
+		return get_point(along);
+	}
+	
+	public function set_and_get(_screen_pos:Vector, along:Float):Vector {
+		screen_pos = _screen_pos;
+		return get_point(along);
+	}
+}
