2b0d747f57b484f9a65b5e0512a1a1c6034fbc75
underscorediscovery <sven@underscorediscovery.com>
2015-04-08 05:40:04 -0230

    tests; nineslice; update to use asset image instead of embedded image.

--- tests/features/nineslice/src/Main.hx
+++ tests/features/nineslice/src/Main.hx
@@ -19,39 +19,50 @@ class Main extends luxe.Game {
 
         Luxe.renderer.clear_color = new Color().rgb(0xefefef);
 
-        geom = new NineSlice({
-            texture : Luxe.loadTexture('tiny.ui.png'),
-            top : 32, left : 32, right : 32, bottom : 32,
-            color : new Color(1,1,1,0.4)
-        });
+        Luxe.loadTexture('assets/tiny.ui.png', function(tex){
 
-        geom.create( new Vector(100, 200), 500, 260 );
+            geom = new NineSlice({
+                texture : tex,
+                top : 32, left : 32, right : 32, bottom : 32,
+                color : new Color(1,1,1,0.4)
+            });
+
+            geom.create( new Vector(100, 200), 500, 260 );
+
+        });
 
     } //ready
 
     override function onmousemove( e:MouseEvent ) {
 
-        var m = new Vector(e.x,e.y);
-        geom.pos = m;
+        if(geom != null) {
+            geom.pos = new Vector(e.x,e.y);
+        }
 
     } //onmousemove
 
     function sizechange() {
 
-        geom.size = new Vector(sx,sy);
+        if(geom != null) {
+            geom.size = new Vector(sx,sy);
+        }
 
     } //sizechange
 
     override function onkeyup( e:KeyEvent ) {
 
-        if(e.keycode == Key.space) {
-            var nsx : Int = Math.round( 100+(Math.random()*600) );
-            var nsy : Int = Math.round( 100+(Math.random()*600) );
-            Actuate.tween( this , 0.2 , { sx:nsx, sy:nsy } , true ).onUpdate(sizechange);
+        if(geom != null) {
 
-            var new_a = luxe.utils.Maths.clamp(0.1 + Math.random(), 0, 1);
-            Actuate.tween( geom.color, 0.2 , { a:new_a }, true );
-        }
+            if(e.keycode == Key.space) {
+                var nsx : Int = Math.round( 100+(Math.random()*600) );
+                var nsy : Int = Math.round( 100+(Math.random()*600) );
+                Actuate.tween( this , 0.2 , { sx:nsx, sy:nsy } , true ).onUpdate(sizechange);
+
+                var new_a = luxe.utils.Maths.clamp(0.1 + Math.random(), 0, 1);
+                Actuate.tween( geom.color, 0.2 , { a:new_a }, true );
+            }
+
+        } //geom
 
         if(e.keycode == Key.escape) {
             Luxe.shutdown();
