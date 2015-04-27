84b2d0fa9bd3bd2f74819610848ac1ce829e3274
underscorediscovery <sven@underscorediscovery.com>
2015-04-27 15:04:39 -0230

    Resource; new Resource handler (see extended details)
        - added `reload` api handled by all resource types
        - added `TextResource` `JSONResource` `BytesResource`
            - these all contain a snow `Asset` type
            - which means:
                - previously `res.text`, `res.json` and `res.bytes` becomes
                - `res.asset.text`, `res.asset.json` and `res.asset.bytes`
        - added reference counting for handling `destroy` and sharing
            - tracked by `Resources` for you on `load` calls
            - will be used in subsequent additions re: parcel diff features
        - added resource `state` property, for `listed`, `loading`,
    `loaded`, `failed` etc
        - added invalidation and clearing
            - invalidate triggers an event, usually from some other event
    (like file change, context loss)
            - clear destroys asset/gpu/memory/other resources and slims
    down the instance to just state
            - clear is auto handled on reload, invalidate, and others

--- luxe/resource/Resource.hx
+++ luxe/resource/Resource.hx
@@ -1,131 +1,346 @@
 package luxe.resource;
 
-import snow.api.buffers.Uint8Array;
+import luxe.Log.*;
+import luxe.options.ResourceOptions;
+import luxe.Resources;
 
-import luxe.resource.Resources;
-
-
-@:enum
-abstract ResourceType(Int) from Int to Int {
-    var unknown = 0;
-    var text = 1;
-    var json = 2;
-    var data = 3;
-    var texture = 4;
-    var sound = 5;
-    var render_texture = 6;
-    var font = 7;
-    var shader = 8;
-}
+import snow.api.Promise;
+import snow.system.assets.Asset;
 
 class Resource {
 
+        /** The unique resource ID.
+            If the resource is loaded from disk, this matches the asset id.
+            If not from disk, the id can be anything but must be unique to this resource. */
+    public var id: String;
+        /** The resource system that contains this Resource. */
+    public var system: Resources;
+        /** The resource type ID. Defaults use ResourceType ids but is typed Int for user id's. */
+    public var resource_type: Int;
+        /** The state this resource is currently in. */
+    public var state (default, set): ResourceState;
 
-    public var manager : Resources;
-    public var type : ResourceType;
-    public var id : String;
-    public var persistent : Bool = false;
-    public var time_to_load : Float = 0;
-    public var time_created : Float = 0;
-    public var dropped : Bool = false;
+        /** The reference count of this resource. Each time a
+            `load_*` call is made on the resource system, it increments
+            the count specific to that resource. When a resource calls
+            `destroy()` it decreases by one. When the ref count is 0, and the
+            resource is destroyed, it's properly destroyed.
+            This works via setter, so changing it with ref++ and ref-- is ok. */
+    public var ref (default, set): Int = 0;
 
+    public function new( _options:ResourceOptions ) {
 
-    public function new( _manager : Resources, _type:ResourceType, ?_load_time:Float ) {
+        assertnull(_options);
+        assertnull(_options.id);
 
-        manager = _manager == null ? Luxe.resources : _manager;
-        type = _type;
+        if(_options.system == null) _options.system = Luxe.resources;
+        if(_options.resource_type == null) _options.resource_type = ResourceType.unknown;
 
-        time_to_load = _load_time;
-        time_created = Luxe.time;
-
-        manager.add( this );
+        id = _options.id;
+        system = _options.system;
+        resource_type = _options.resource_type;
+        state = ResourceState.unknown;
+        ref = 1;
 
     } //new
 
-    public function drop() {
-        if(!dropped) {
-            dropped = true;
-            manager.remove( this );
+        /** Destroys the resource, unlisting it, cleaning up it's resource usage.
+            Once destroyed, it is no longer valid and should not be used in any way.
+            To clean up without destroying the resource, use invalidate().
+            To force destroying and ignore the ref counting, pass true for `force` flag. */
+    public function destroy( ?_force:Bool=false ) : Void {
+
+        assert(state != ResourceState.destroyed);
+
+        if(!_force) {
+            assert(ref > 0);
+            ref--;
+            assert(ref >= 0);
         }
-    } //drop
+
+        _debug('destroy / $id  / ref $ref');
+
+        if(ref == 0 || _force) {
+
+            clear();
+            state = destroyed;
+            system.remove(this);
+            system.emit(destroyed, this);
+
+        } //no refs || force
+
+    } //destroy
+
+        /** Invalidates and frees up any resource usage held.
+            The resource instance remains valid for use, and remains listed in the resource cache. */
+    public function invalidate() : Void {
+
+        assert(state != ResourceState.destroyed);
+
+        clear();
+        state = invalidated;
+        system.emit(invalidated, this);
+
+    }
+
+        /** Returns a promise for the reloaded resource.
+            Returns null from the base `Resource` type, implemented in subclasses. */
+    public function reload() : Promise {
+        return null;
+    }
+
+        /** Returns the estimated memory use by this resource, in bytes.
+            You can use Luxe.utils.bytes_to_string to display this value easily.
+            Some resources return 0, in the case of not being able to give an estimate. */
+    public function memory_use() : Float {
+        return 0;
+    } //memory_use
+
+//Internal
+
+    function set_ref( _ref:Int ) {
+
+        var pre = ref;
+        ref = _ref;
+
+        if(ref > pre) {
+            system.emit(ResourceEvent.reference, this);
+        } else if(ref < pre) {
+            system.emit(ResourceEvent.dereference, this);
+        }
+
+        return ref;
+
+    } //set_ref
+
+    function set_state( _state:ResourceState ) {
+
+        state = _state;
+
+        switch(state) {
+            case ResourceState.loading:
+                system.emit(ResourceEvent.loading, this);
+            case ResourceState.loaded:
+                system.emit(ResourceEvent.loaded, this);
+            case ResourceState.failed:
+                system.emit(ResourceEvent.failed, this);
+            case _:
+        }
+
+        return state;
+
+    } //set_state
+
+        /** Clean up any resource usage, freeing memory, removing references etc. */
+    function clear() : Void {
+
+    } //
+
+
+    function state_string() {
+        return switch(state) {
+            case ResourceState.listed:      'listed';
+            case ResourceState.loading:     'loading';
+            case ResourceState.loaded:      'loaded';
+            case ResourceState.failed:      'failed';
+            case ResourceState.invalidated: 'invalidated';
+            case ResourceState.destroyed:   'destroyed';
+            case _:                         'unknown';
+        }
+    }
+
+    function type_string() {
+        return switch(resource_type) {
+            case ResourceType.bytes:    'bytes';
+            case ResourceType.text:     'text';
+            case ResourceType.json:     'json';
+            case ResourceType.texture:  'texture';
+            case ResourceType.shader:   'shader';
+            case ResourceType.font:     'font';
+            case _: '$resource_type';
+        }
+    }
+
+    function toString() {
+        return 'Resource(`$id`, ${type_string()}, ${state_string()})';
+    }
 
 
 } //Resource
 
 
-class TextResource extends Resource {
+class BytesResource extends Resource {
 
+    public var asset:AssetBytes;
 
-    public var text : String;
+    public function new( _options:BytesResourceOptions ) {
 
+        assertnull(_options);
 
-    public function new( _id:String, _text:String, _manager:Resources ) {
+        _options.resource_type = ResourceType.bytes;
 
-        id = _id;
+        super( _options );
 
-        super( _manager, ResourceType.text );
-
-        text = _text;
+        asset = _options.asset;
 
     } //new
 
+    override function reload() : Promise {
+
+        assert(state != ResourceState.destroyed);
+
+        clear();
+
+        return new Promise(function(resolve, reject) {
+
+            state = ResourceState.loading;
+
+            var get = Luxe.snow.assets.bytes(id);
+
+            get.then(function(_asset:AssetBytes) {
+
+                asset = _asset;
+                state = ResourceState.loaded;
+                resolve(this);
+
+            });
+
+            get.error(reject);
+
+        }); //promise
+
+    } //reload
+
+    override function clear() {
+
+        if(asset != null) {
+            asset.destroy();
+            asset = null;
+        }
+
+    } //clear
+
+    override function memory_use() {
+
+        if(asset == null) return 0;
+
+        return asset.bytes.byteLength;
+
+    } //memory_use
+
+} //BytesResource
+
+class TextResource extends Resource {
+
+    public var asset:AssetText;
+
+    public function new( _options:TextResourceOptions ) {
+
+        assertnull(_options);
+
+        _options.resource_type = ResourceType.text;
+
+        super( _options );
+
+        asset = _options.asset;
+
+    } //new
+
+    override function reload() : Promise {
+
+        assert(state != ResourceState.destroyed);
+
+        clear();
+
+        return new Promise(function(resolve, reject) {
+
+            state = ResourceState.loading;
+
+            var get = Luxe.snow.assets.text(id);
+
+            get.then(function(_asset:AssetText) {
+
+                asset = _asset;
+                state = ResourceState.loaded;
+                resolve(this);
+
+            });
+
+            get.error(reject);
+
+        }); //promise
+
+    } //reload
+
+    override function clear() {
+
+        if(asset != null) {
+            asset.destroy();
+            asset = null;
+        }
+
+    } //clear
+
+    override function memory_use() {
+
+        if(asset == null) return 0;
+
+        return asset.text.length;
+
+    } //memory_use
 
 } //TextResource
 
 class JSONResource extends Resource {
 
+    public var asset:AssetJSON;
 
-    public var json : Dynamic;
+    public function new( _options:JSONResourceOptions ) {
 
+        assertnull(_options);
 
-    public function new( _id:String, _json:Dynamic, _manager:Resources ) {
+        _options.resource_type = ResourceType.json;
 
-        id = _id;
+        super( _options );
 
-        super( _manager, ResourceType.json );
-
-        json = _json;
+        asset = _options.asset;
 
     } //new
+
+    override function reload() : Promise {
+
+        assert(state != ResourceState.destroyed);
+
+        clear();
+
+        return new Promise(function(resolve, reject) {
+
+            state = ResourceState.loading;
+
+            var get = Luxe.snow.assets.json(id);
+
+            get.then(function(_asset:AssetJSON) {
+
+                asset = _asset;
+                state = ResourceState.loaded;
+                resolve(this);
+
+            });
+
+            get.error(reject);
+
+        }); //promise
+
+    } //reload
+
+    override function clear() {
+
+        if(asset != null) {
+            asset.destroy();
+            asset = null;
+        }
+
+    } //clear
 
 
 } //JSONResource
-
-class DataResource extends Resource {
-
-
-    public var data : Uint8Array;
-
-
-    public function new( _id:String, _data:Uint8Array, _manager:Resources ) {
-
-        id = _id;
-
-        super( _manager, ResourceType.data );
-
-        data = _data;
-
-    } //new
-
-
-} //DataResource
-
-
-class SoundResource extends Resource {
-
-
-    public var name : String;
-
-
-    public function new( _id:String, _name:String, _manager:Resources ) {
-
-        id = _id;
-
-        super( _manager, ResourceType.sound );
-
-        name = _name;
-
-    } //new
-
-
-} //SoundResource
