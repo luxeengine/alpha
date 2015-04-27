eaa21faedd73b5a1f58ef79e27dc35f2d428ab6b
underscorediscovery <sven@underscorediscovery.com>
2015-04-27 15:05:43 -0230

    Debug; StatsView; update to display the resource list more effectively, and better formatted, displays reference counts, id's and more, includes an automatic slide to display out-of-view content and doesn't fold and wrap outside of the debug console frame any more

--- luxe/debug/StatsDebugView.hx
+++ luxe/debug/StatsDebugView.hx
@@ -1,19 +1,14 @@
 package luxe.debug;
 
 import luxe.Input;
+import luxe.Text;
 import luxe.Screen.WindowEvent;
 import luxe.resource.Resource;
-
-typedef RenderStats = {
-    batchers : Int,
-    geometry_count : Int,
-    dynamic_batched_count : Int,
-    static_batched_count : Int,
-    visible_count : Int,
-    draw_calls : Int,
-    vert_count : Int,
-    group_count : Int
-}
+import luxe.Resources;
+import luxe.tween.Actuate;
+import luxe.utils.Maths;
+import phoenix.Shader;
+import phoenix.Texture;
 
 class StatsDebugView extends luxe.debug.DebugView  {
 
@@ -26,7 +21,6 @@ class StatsDebugView extends luxe.debug.DebugView  {
 
         //stats console
     public var render_stats_text : luxe.Text;
-    public var resource_stats_text : luxe.Text;
     public var resource_list_text : luxe.Text;
 
     public function new() {
@@ -57,24 +51,29 @@ class StatsDebugView extends luxe.debug.DebugView  {
             group_count : 0
         };
 
-    }
+    } //new
 
     public function get_resource_stats_string() {
+
         return Std.string( Luxe.resources.stats );
-    }
+
+    } //get_resource_stats_string
 
     public function get_render_stats_string() {
-        return
-            'Renderer Statistics\n' +
-            '\tbatcher count : ' + _render_stats.batchers + '\n' +
-            '\ttotal geometry : ' + _render_stats.geometry_count + '\n' +
-            '\tvisible geometry : ' + _render_stats.visible_count + '\n' +
-            '\tdynamic batch count : ' + _render_stats.dynamic_batched_count + '\n' +
-            '\tstatic batch count : ' + _render_stats.static_batched_count + '\n' +
-            '\ttotal draw calls : ' + _render_stats.draw_calls + '\n' +
-            '\ttotal vert count : ' + _render_stats.vert_count;
-    }
 
+        return
+            'Renderer Statistics\n\n' +
+            'batcher count : ' + _render_stats.batchers + '\n' +
+            'total geometry : ' + _render_stats.geometry_count + '\n' +
+            'visible geometry : ' + _render_stats.visible_count + '\n' +
+            'dynamic batch count : ' + _render_stats.dynamic_batched_count + '\n' +
+            'static batch count : ' + _render_stats.static_batched_count + '\n' +
+            'total draw calls : ' + _render_stats.draw_calls + '\n' +
+            'total vert count : ' + _render_stats.vert_count;
+
+    } //get_render_stats_string
+
+    var margin = 32;
     public override function create() {
 
         var debug = Luxe.debug;
@@ -83,96 +82,126 @@ class StatsDebugView extends luxe.debug.DebugView  {
             depth : 999.3,
             no_scene : true,
             color : new Color(0,0,0,1).rgb(0xf6007b),
-            pos : new Vector(debug.padding.x*2,debug.padding.y*3),
+            pos : new Vector(0,0),
             font : Luxe.renderer.font,
             text : get_render_stats_string(),
             point_size : font_size,
             batcher : debug.batcher,
-            visible : false
-        });
-
-        resource_stats_text = new luxe.Text({
-            depth : 999.3,
-            no_scene : true,
-            color : new Color(0,0,0,1).rgb(0xf6007b),
-            pos : new Vector(debug.padding.x*2,debug.padding.y*7.5),
-            font : Luxe.renderer.font,
-            text : get_resource_stats_string(),
-            point_size : font_size,
-            batcher : debug.batcher,
-            visible : false
+            visible : false,
         });
 
         resource_list_text = new luxe.Text({
             depth : 999.3,
             no_scene : true,
             color : new Color(0,0,0,1).rgb(0xf6007b),
-            pos : new Vector(debug.padding.x*7,debug.padding.y*3),
+            pos : new Vector(0,0),
             font : Luxe.renderer.font,
             text : '',
-            point_size : font_size*0.8,
+            align : TextAlign.right,
+            point_size : font_size*0.9,
             batcher : debug.batcher,
-            visible : false
+            visible : false,
         });
 
+        resize();
 
-        resource_list_text.locked = true;
-        resource_stats_text.locked = true;
+        Luxe.resources.on(ResourceEvent.any, function(_) if(visible) refresh() );
 
-    }
+    } //create
 
-    override function onwindowsized(e:WindowEvent) {
+    function resize() {
+
+        if( resource_list_text == null ||
+            render_stats_text == null) {
+            return;
+        }
 
         var debug = Luxe.debug;
 
+        var viewrect = new Rectangle(
+            debug.debug_inspector.pos.x + (margin/2),
+            debug.debug_inspector.pos.y + (margin*1.5),
+            debug.debug_inspector.size.x - margin,
+            debug.debug_inspector.size.y - margin - (margin*1.5)
+        );
+
+        var left = debug.padding.x + margin;
+        var right = debug.padding.x + debug.debug_inspector.size.x - margin;
+        var top = debug.padding.y +(margin*1.5);
+        var render_w = render_stats_text.text_bounds.w;
+        var render_h = render_stats_text.text_bounds.h;
+
         if(resource_list_text != null) {
-            resource_list_text.pos = new Vector(debug.padding.x*7,debug.padding.y*3);
-            resource_list_text.geometry.dirty = true;
-        }
-        if(resource_stats_text != null) {
-            resource_stats_text.pos = new Vector(debug.padding.x*2,debug.padding.y*7.5);
-            resource_stats_text.geometry.dirty = true;
-        }
-        if(render_stats_text != null) {
-            render_stats_text.pos = new Vector(debug.padding.x*2,debug.padding.y*3);
-            render_stats_text.geometry.dirty = true;
+            resource_list_text.pos = new Vector(right, top);
+            resource_list_text.clip_rect = viewrect;
         }
 
-    } //onwindowsized
+        if(render_stats_text != null) {
+            render_stats_text.pos = new Vector(left, top);
+            render_stats_text.clip_rect = viewrect;
+        }
+
+        reset_tween();
+
+    }
+
+    override function onwindowsized(e:WindowEvent) resize();
 
     public override function refresh() {
 
+        var bytes_lists = '';
+        var text_lists = '';
+        var json_lists = '';
         var texture_lists = '';
-        var shader_lists = '';
+        var rtt_lists = '';
         var font_lists = '';
+        var shader_lists = '';
 
-        for(res in Luxe.resources.resourcelist) {
-            switch (res.type) {
-                case ResourceType.texture:
-                    var t : phoenix.Texture = cast res;
-                    texture_lists += '\t' + t.id + '    (' +  t.width_actual + 'x' + t.height_actual + '  '+ t.estimated_memory() +' )\n';
-                case ResourceType.font:
-                    font_lists += '\t' + res.id + '\n';
-                case ResourceType.shader:
-                    shader_lists += '\t' + res.id + '\n';
+        inline function _res(res:Resource) return '${res.id} • ${res.ref}\t\n';
+        inline function _tex(tex:Texture) return '(${tex.width_actual}x${tex.height_actual} ~${Luxe.utils.bytes_to_string(tex.memory_use())})    ${tex.id} • ${tex.ref}\t\n';
+        inline function _shd(res:Shader) return '(${res.vert_id}, ${res.frag_id})    ${res.id} • ${res.ref}\t\n';
+
+        for(res in Luxe.resources.cache) {
+            switch(res.resource_type) {
+                case ResourceType.bytes:            bytes_lists += _res(res);
+                case ResourceType.text:             text_lists += _res(res);
+                case ResourceType.json:             json_lists += _res(res);
+                case ResourceType.texture:          texture_lists += _tex(cast res);
+                case ResourceType.render_texture:   rtt_lists += _tex(cast res);
+                case ResourceType.font:             font_lists += _res(res);
+                case ResourceType.shader:           shader_lists += _shd(cast res);
                 default:
             }
         }
 
-        var lists = 'Fonts\n';
-            lists += font_lists;
-            lists += 'Shader\n';
-            lists += shader_lists;
-            lists += 'Textures\n';
-            lists += texture_lists;
+        inline function orblank(v:String) return (v == '') ? '-\t\n' : v;
+
+        var lists = 'Resource list (${Luxe.resources.stats.total})\n\n';
+
+            lists += 'Bytes (${Luxe.resources.stats.bytes})\n';
+                lists += orblank(bytes_lists);
+            lists += '\nText (${Luxe.resources.stats.bytes})\n';
+                lists += orblank(text_lists);
+            lists += '\nJSON (${Luxe.resources.stats.jsons})\n';
+                lists += orblank(json_lists);
+            lists += '\nTexture (${Luxe.resources.stats.textures})\n';
+                lists += orblank(texture_lists);
+            lists += '\nRenderTexture (${Luxe.resources.stats.render_textures})\n';
+                lists += orblank(rtt_lists);
+            lists += '\nFont (${Luxe.resources.stats.fonts})\n';
+                lists += orblank(font_lists);
+            lists += '\nShader (${Luxe.resources.stats.shaders})\n';
+                lists += orblank(shader_lists);
 
         resource_list_text.text = lists;
+
         if(resource_list_text.geometry != null) {
             resource_list_text.geometry.dirty = true;
         }
-    }
 
+        reset_tween();
 
+    } //refresh
 
     public override function process() {
 
@@ -206,26 +235,74 @@ class StatsDebugView extends luxe.debug.DebugView  {
 
     } //process
 
+#if desktop
+    //:wip:
+    override function onmousewheel(e:MouseEvent) {
+
+        Actuate.stop(resource_list_text.pos);
+
+        var h = resource_list_text.text_bounds.h;
+        var vh = Luxe.debug.debug_inspector.size.y - margin;
+        var diff = h - vh;
+
+        var new_y = resource_list_text.pos.y;
+        var max_y = Luxe.debug.padding.y +(margin*1.5);
+        var min_y = max_y;
+
+        if(diff > 0) {
+            min_y = (max_y - (diff+(margin*2)));
+        }
+
+        new_y -= (margin/2) * e.y;
+        new_y = Maths.clamp(new_y, min_y, max_y);
+        resource_list_text.pos.y = new_y;
+
+    }
+#end
+
     public override function onkeydown(e:KeyEvent) {
-        if(e.keycode == Key.key_2 && visible ) {
+
+        if(e.keycode == Key.key_2 && visible) {
             toggle_debug_stats();
         }
+
     } //onkeydown
 
     public override function show() {
+
         super.show();
         refresh();
         render_stats_text.visible = true;
-        resource_stats_text.visible = true;
         resource_list_text.visible = true;
+
     } //show
 
     public override function hide() {
+
         super.hide();
         render_stats_text.visible = false;
-        resource_stats_text.visible = false;
         resource_list_text.visible = false;
+        Actuate.stop(resource_list_text.pos);
+
     } //hide
+
+    function reset_tween() {
+
+        Actuate.stop(resource_list_text.pos);
+
+        var h = resource_list_text.text_bounds.h;
+        var vh = Luxe.debug.debug_inspector.size.y - margin;
+        var diff = h - vh;
+
+        var start_y = Luxe.debug.padding.y +(margin*1.5);
+        resource_list_text.pos.y = start_y;
+
+        if(diff > 0) {
+            var end_y = (start_y - (diff+(margin*2)));
+            Actuate.tween(resource_list_text.pos, 8, { y:end_y }).repeat().delay(4).reflect().ease(luxe.tween.easing.Linear.easeNone);
+        }
+
+    }
 
    public function refresh_render_stats() {
 
@@ -234,13 +311,10 @@ class StatsDebugView extends luxe.debug.DebugView  {
         }
 
         render_stats_text.text = get_render_stats_string();
-        resource_stats_text.text = get_resource_stats_string();
 
-        resource_stats_text.locked = true;
         render_stats_text.locked = true;
 
         if(render_stats_text.geometry != null) {
-            resource_stats_text.geometry.dirty = true;
             render_stats_text.geometry.dirty = true;
         }
 
@@ -248,8 +322,10 @@ class StatsDebugView extends luxe.debug.DebugView  {
 
     public var hide_debug : Bool = true;
     public function toggle_debug_stats() {
+
          hide_debug = !hide_debug;
-    }
+
+    } //toggle_debug_stats
 
     public function update_render_stats() {
 
@@ -280,4 +356,13 @@ class StatsDebugView extends luxe.debug.DebugView  {
 
 } //StatsDebugView
 
-
+typedef RenderStats = {
+    batchers : Int,
+    geometry_count : Int,
+    dynamic_batched_count : Int,
+    static_batched_count : Int,
+    visible_count : Int,
+    draw_calls : Int,
+    vert_count : Int,
+    group_count : Int
+}
