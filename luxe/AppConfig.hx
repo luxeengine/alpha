aa8e1ec6d9a0482ea0aac19b9be56726b9955b09
underscorediscovery <sven@underscorediscovery.com>
2015-04-27 14:45:32 -0230

    AppConfig; add preload parcel options

--- luxe/AppConfig.hx
+++ luxe/AppConfig.hx
@@ -1,3 +1,16 @@
 package luxe;
 
-typedef AppConfig = snow.types.Types.AppConfig;
+import luxe.Parcel;
+
+typedef AppConfig = {
+    > snow.types.Types.AppConfig,
+    ? preload: {
+        ? bytes: Array<BytesInfo>,
+        ? texts: Array<TextInfo>,
+        ? jsons: Array<JSONInfo>,
+        ? textures: Array<TextureInfo>,
+        ? fonts: Array<BitmapFontInfo>,
+        ? shaders: Array<ShaderInfo>,
+        ? sounds: Array<SoundInfo>
+    }
+}
