eb0f23f50d61bd930ee1d1fcb52dbe6abbef99d0
underscorediscovery <sven@underscorediscovery.com>
2015-05-09 19:05:10 -0230

    Objects; allow name and id to be overridden as a property

--- luxe/Objects.hx
+++ luxe/Objects.hx
@@ -21,9 +21,9 @@ class ID {
 class Objects extends Emitter<Int> {
 
         /** the id */
-    public var id : String = '';
+    @:isVar public var id (get,set) : String = '';
         /** the name */
-    public var name : String = '';
+    @:isVar public var name (get,set) : String = '';
 
     @:noCompletion public function new(?_name:String='', _id:String='') {
 
@@ -32,5 +32,13 @@ class Objects extends Emitter<Int> {
         id = _id == '' ? Luxe.utils.uniqueid() : _id;
 
     } //new
+
+//These can't be inlined because they are intended to be overridden in subclasses
+
+    function set_name(_name:String) : String    return name = _name;
+    function set_id(_id:String) : String        return id = _id;
+    function get_name() : String                return name;
+    function get_id() : String                  return id;
+
 
 } //Objects
