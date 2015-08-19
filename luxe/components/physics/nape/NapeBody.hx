0cd40dd0da4ee55056b163b3d3e5302a524559bf
underscorediscovery <sven@underscorediscovery.com>
2015-08-19 09:22:58 -0230

    Physics; Nape; Add collider components for convenience, along with a simple example. Initially implemented by @whuop, finalized for the repo by me

---
+++ luxe/components/physics/nape/NapeBody.hx
@@ -0,0 +1,78 @@
+package luxe.components.physics.nape;
+
+import luxe.Vector;
+import luxe.Component;
+import luxe.options.ComponentOptions;
+
+typedef NapeBodyOptions = {
+
+	> ComponentOptions,
+
+        /** Set internally unless creating a NapeBody */
+    @:optional var body_type : nape.phys.BodyType;
+        /** The optional InteractionFilter */
+    @:optional var filter : nape.dynamics.InteractionFilter;
+        /** The optional nape material */
+	@:optional var material : nape.phys.Material;
+
+} //NapeBodyOptions
+
+class NapeBody extends Component {
+
+	public var body(default, default) : nape.phys.Body;
+
+    var body_options : NapeBodyOptions;
+    var euler : Vector;
+
+	public function new(_options : NapeBodyOptions) : Void {
+
+        body_options = _options;
+        euler = new Vector();
+
+		super(body_options);
+
+	} //new
+
+    override function onadded() : Void {
+        
+        body = new nape.phys.Body(body_options.body_type);
+
+    } //onadded
+
+    override function onremoved() : Void {
+
+        super.onremoved();
+
+        if(Luxe.physics.nape.debugdraw != null) {
+            Luxe.physics.nape.debugdraw.remove(body);
+        }
+
+        body.space = null;
+        body = null;
+
+    } //onremoved
+
+    override function update(dt:Float) {
+
+        if(entity != null) {
+            entity.pos.set_xy(body.position.x, body.position.y);
+            euler.z = body.rotation;
+            entity.rotation.setFromEuler(euler);
+        }
+
+    } //update
+
+//internal
+    
+        //stuff that should happen after being setup in onadded
+    function post_add() {
+
+        body.space = Luxe.physics.nape.space;
+
+        if(Luxe.physics.nape.debugdraw != null) {
+            Luxe.physics.nape.debugdraw.add(body);
+        }
+    
+    } //post_add
+
+} //Body
---
+++ luxe/components/physics/nape/PolygonCollider.hx
@@ -0,0 +1,52 @@
+package luxe.components.physics.nape;
+
+import nape.geom.Vec2;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.shape.Polygon;
+
+import luxe.Vector;
+import luxe.components.physics.nape.NapeBody;
+
+typedef PolygonColliderOptions = {
+
+    > NapeBodyOptions,
+
+        /** the x position of the circle */
+    var x : Float;
+        /** the y position of the circle */
+    var y : Float; 
+        /** The local vertices of the polygon */
+    var polygon : Array<Vector>;
+
+} //PolygonColliderOptions
+
+class PolygonCollider extends NapeBody {
+
+    var options : PolygonColliderOptions;
+
+    public function new(_options : PolygonColliderOptions) : Void {
+
+        options = _options;
+
+        super(options);
+
+    } //new
+
+    override function onadded() {
+
+        super.onadded();
+
+            var verts = [];
+            for(point in options.polygon) {
+                verts.push( new Vec2(point.x, point.y) );
+            }
+
+            body.shapes.add(new Polygon(verts, options.material, options.filter));
+            body.position.setxy(options.x, options.y);
+        
+        post_add();
+
+    } //onadded
+
+} //PolygonCollider
